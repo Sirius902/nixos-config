fdc1e35516e6352688905f38ad85ce06e6c07e5e
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-07-22 22:21:58 -0700

    Set up hee-ho

--- hosts/hee-ho.nix
+++ hosts/hee-ho.nix
@@ -1,3 +1,50 @@
+{ pkgs, ... }:
+
 {
   boot.zfs.extraPools = [ "data" ];
+
+  # Allow ports for mc.
+  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedUDPPorts = [ 25565 ];
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 * * * *    chris    /media/data/mc/backup-all.sh"
+    ];
+  };
+
+  # TODO: Get systemd service to work. Use this instead of cron job.
+  # systemd.timers."mc-backup" = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig = {
+  #     OnBootSec = "1h";
+  #     OnUnitActiveSec = "1h";
+  #     Unit = "mc-backup.service";
+  #   };
+  # };
+  #
+  # systemd.services."mc-backup" = {
+  #   path = [
+  #     pkgs.bash
+  #     pkgs.gawk
+  #     pkgs.gnutar
+  #     pkgs.screen
+  #   ];
+  #   script = ''
+  #     set -eu
+  #     ${pkgs.bash}/bin/bash /media/data/mc/backup-all.sh
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     User = "chris";
+  #     Group = "users";
+  #     IgnoreSIGPIPE = false;
+  #   };
+  # };
+
+  environment.systemPackages = with pkgs; [
+    screen
+    temurin-jre-bin
+  ];
 }
