7f83beb6741da009a992d464ccd5a988c4a7ef5d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-09-24 16:23:46 -0700

    Add ZSA stuff

--- modules/desktop-common.nix
+++ modules/desktop-common.nix
@@ -6,9 +6,12 @@
     binfmt = true;
   };
 
+  hardware.keyboard.zsa.enable = true;
+
   environment.systemPackages = with pkgs; [
     gparted
     jetbrains.idea-community
+    keymapp
     krita
     nautilus-python # Required for Open in WezTerm.
     qdirstat
