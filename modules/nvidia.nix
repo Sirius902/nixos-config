46c56780056f80e1cad72d9176f061427d93b413
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-11-03 14:31:17 -0800

    Switch to NVIDIA 565.57.01

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   # Enable OpenGL
@@ -40,6 +40,13 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "565.57.01";
+      sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+      sha256_aarch64 = lib.fakeSha256;
+      openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+      settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
 }
