e551ae8cddea4177d80210b9bea336cbfa407f4c
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-11-12 15:54:57 -0800

    Add soh

--- hosts/nixlee.nix
+++ hosts/nixlee.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../modules/desktop-common.nix
     ../modules/secure-boot.nix
     ../modules/documentation.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    shipwright
   ];
 }
