65b827de3c2c478ec24c1ecfe8631a585f427577
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-12-23 23:37:10 -0800

    Add xrdp

---
+++ modules/xrdp.nix
@@ -0,0 +1,8 @@
+{pkgs, ...}: {
+  # NOTE(Sirius902) Make sure you're logged out of GNOME on the target machine otherwise
+  # you'll arrive at a black screen.
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+  services.xrdp.openFirewall = true;
+}
