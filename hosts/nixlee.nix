cfb9dc22041587cbc528d970e709db4f617b257e
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-01-22 18:26:58 -0800

    chore: Use filesystems for steam vm mount

--- hosts/nixlee.nix
+++ hosts/nixlee.nix
@@ -14,17 +14,10 @@
     shipwright
   ];
 
-  systemd.services."bind-media-vm-shared-steam" = {
-    description = "Bind mount /media/steam to /media/vm/shared/steam";
-    after = ["zfs-mount.service"];
-    requires = ["zfs-mount.service"];
-    wantedBy = ["local-fs.target"];
-
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = ["${pkgs.util-linux}/bin/mount --rbind /media/steam /media/vm/shared/steam"];
-      ExecStop = ["${pkgs.util-linux}/bin/umount /media/vm/shared/steam"];
-      RemainAfterExit = true; # Keeps the mount active after the unit runs.
-    };
+  # Mount Steam under shared directory for VMs to access
+  fileSystems."/media/vm/shared/steam" = {
+    device = "/media/steam";
+    fsType = "none";
+    options = ["rbind" "x-systemd.automount"];
   };
 }
