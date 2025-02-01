d7e2644d06da97c4dce6beba91ee6a9ca1115bff
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-02-01 12:37:54 -0800

    chore: remove cosmic fullscreen hack

--- modules/desktop/cosmic/default.nix
+++ modules/desktop/cosmic/default.nix
@@ -5,10 +5,4 @@
   services.desktopManager.cosmic.enable = true;
 
   # FUTURE(Sirius902) Use https://github.com/AndreasBackx/waycorner for workspace overview hot corner?
-
-  # FUTURE(Sirius902) Hack to fix fullscreen freezing issue.
-  # https://github.com/pop-os/cosmic-comp/issues/887#issuecomment-2586099427
-  environment.etc."profile.d/cosmic-temp.sh".text = ''
-    export COSMIC_DISABLE_DIRECT_SCANOUT=1
-  '';
 }
