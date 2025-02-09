743513193e42e29831fa7948d73fae692be2e164
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-02-09 01:11:26 -0800

    feat: add GNOME system monitor to COSMIC

--- modules/desktop/cosmic/default.nix
+++ modules/desktop/cosmic/default.nix
@@ -1,8 +1,16 @@
-{nixos-cosmic, ...}: {
+{
+  nixos-cosmic,
+  pkgs,
+  ...
+}: {
   imports = [nixos-cosmic.nixosModules.default];
 
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+
+  # FUTURE(Sirius902) Use GNOME system monitor since COSMIC doesn't have one yet. Switch to COSMIC
+  # one when/if it exists.
+  environment.systemPackages = with pkgs; [gnome-system-monitor];
 
   # FUTURE(Sirius902) Use https://github.com/AndreasBackx/waycorner for workspace overview hot corner?
 }
