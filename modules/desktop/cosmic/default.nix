faa256b9961308ee9530109fc692264f0efd1ef6
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-04-30 13:38:58 -0700

    feat: switch to cosmic-observatory

--- modules/desktop/cosmic/default.nix
+++ modules/desktop/cosmic/default.nix
@@ -8,9 +8,12 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
-  # FUTURE(Sirius902) Use GNOME system monitor since COSMIC doesn't have one yet. Switch to COSMIC
-  # one when/if it exists.
-  environment.systemPackages = with pkgs; [gnome-system-monitor];
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+
+  environment.systemPackages = [pkgs.observatory];
+
+  systemd.packages = [pkgs.observatory];
+  systemd.services.monitord.wantedBy = ["multi-user.target"];
 
   # FUTURE(Sirius902) Use https://github.com/AndreasBackx/waycorner for workspace overview hot corner?
 }
