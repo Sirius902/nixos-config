81fc85a26112270391499944cf4c77ec728c664b
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-05-15 14:39:21 -0700

    feat: linux kernel 6.13 -> 6.14

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@
   boot.loader.efi.efiSysMountPoint = "/efi";
 
   # Only use kernel versions supported by ZFS.
-  boot.kernelPackages = pkgs.linuxPackages_6_13;
+  boot.kernelPackages = pkgs.linuxPackages_6_14;
   boot.zfs.package = pkgs.zfs_2_3;
 
   # Disable hibernation.
