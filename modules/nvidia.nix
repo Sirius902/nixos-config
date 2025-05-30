db386a3523b6141dd9e5aca2178abc9f49cb26cd
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-05-29 20:42:41 -0700

    feat: nvidia 575.57.08

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   hardware.graphics = {
@@ -42,12 +43,12 @@
     # TODO(Sirius902) Switch back to production once https://github.com/NixOS/nixpkgs/pull/408782 gets in.
     # package = config.boot.kernelPackages.nvidiaPackages.production;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.153.02";
-      sha256_64bit = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
-      sha256_aarch64 = "sha256-FKhtEVChfw/1sV5FlFVmia/kE1HbahDJaxTlpNETlrA=";
-      openSha256 = "sha256-2DpY3rgQjYFuPfTY4U/5TcrvNqsWWnsOSX0f2TfVgTs=";
-      settingsSha256 = "sha256-5m6caud68Owy4WNqxlIQPXgEmbTe4kZV2vZyTWHWe+M=";
-      persistencedSha256 = "sha256-OSo4Od7NmezRdGm7BLLzYseWABwNGdsomBCkOsNvOxA=";
+      version = "575.57.08";
+      sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
+      sha256_aarch64 = lib.fakeHash;
+      openSha256 = lib.fakeHash;
+      settingsSha256 = "sha256-AIeeDXFEo9VEKCgXnY3QvrW5iWZeIVg4LBCeRtMs5Io=";
+      persistencedSha256 = lib.fakeHash;
     };
   };
 }
