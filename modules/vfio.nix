7ab3c5b76530ffc4f87ca70deba40618647c8eed
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-06-18 19:53:23 -0700

    fix: vfio

--- modules/vfio.nix
+++ modules/vfio.nix
@@ -1,4 +1,23 @@
 {
   boot.kernelParams = ["intel_iommu=on" "iommu=pt"];
-  boot.kernelModules = ["vfio-pci"];
+  boot.kernelModules = ["vfio-pci" "vfio" "vfio_iommu_type1"];
+
+  # NOTE(Sirius902) Should be `softdep nvidia pre: vfio-pci` for NVIDIA.
+  # Pick the correct one based on NixOS options?
+  boot.extraModprobeConfig = ''
+    softdep drm pre: vfio-pci
+  '';
+
+  # NOTE(Sirius902) For AMD it seems you must set some weird KVM settings to
+  # get single GPU passthrough to work.
+  # https://forum.level1techs.com/t/vfio-pass-through-working-on-9070xt/227194
+  #
+  # ```xml
+  # <hyperv>
+  #   <vendor_id state='on' value='myv3nd0r1d'/>
+  # </hyperv>
+  # <kvm>
+  #   <hidden state='on'/>
+  # </kvm>
+  # ```
 }
