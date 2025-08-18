79d84a02e144e28112c0184bb2025fe21104c43b
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-18 16:15:45 -0700

    fix: apply crackle fix for specific audio device

--- modules/pipewire/crackle-fix.nix
+++ modules/pipewire/crackle-fix.nix
@@ -1,14 +1,15 @@
 {
-  services.pipewire.extraConfig.pipewire."92-crackle-fix" = {
-    "context.properties" = {
-      "default.clock.quantum" = 1024; # default 1024
-      "default.clock.min-quantum" = 1024; # default 32
-      "default.clock.max-quantum" = 2048; # default 2048
-    };
-    "pulse.properties" = {
-      "pulse.min.req" = "256/48000";
-      "pulse.min.frag" = "256/48000";
-      "pulse.min.quantum" = "256/48000";
-    };
+  services.pipewire.wireplumber.extraConfig."92-hdmi-crackle-fix" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          {"node.nick" = "DELL S2722DGM";}
+        ];
+        actions.update-props = {
+          "node.latency" = "1024/48000";
+          "node.quantum" = 1024;
+        };
+      }
+    ];
   };
 }
