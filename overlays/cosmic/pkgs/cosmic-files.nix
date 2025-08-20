9ff3ce421bf4f2e32e60572bcc9a4269473b7d10
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-20 14:55:08 -0700

    cosmic-files: 1.0.0-alpha.7-unstable-2025-08-17 -> 1.0.0-alpha.7-unstable-2025-08-20
    Diff: https://github.com/pop-os/cosmic-files/compare/16868ca990383c608f871107aef4e4bd75f74f34...a76f7f86853a23d16ab4cc0bd53f701ff34b742e

--- overlays/cosmic/pkgs/cosmic-files.nix
+++ overlays/cosmic/pkgs/cosmic-files.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-files.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-17";
+  version = "1.0.0-alpha.7-unstable-2025-08-20";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "16868ca990383c608f871107aef4e4bd75f74f34";
-    hash = "sha256-C5KtH2TpIEapl4WhNb9QeVPamAlgGYa5c/TXqE2d5q4=";
+    rev = "a76f7f86853a23d16ab4cc0bd53f701ff34b742e";
+    hash = "sha256-SYwGIk5pHoVDNQa1ObkCyyWiZFpGrSST8DV3sNp5xBo=";
   };
 
   cargoHash = "sha256-TDXo0PsDLIBewAasBK82VsG1O0lPqY6g3dBRFsGzF6A=";
@@ -14,9 +14,6 @@ prev.cosmic-files.overrideAttrs (finalAttrs: prevAttrs: {
     inherit (finalAttrs) pname src version;
     hash = finalAttrs.cargoHash;
   };
-
-  # FUTURE(Sirius902) A few tests are broken currently due to `ThumbCfg` stuff. Re-enable once they are fixed.
-  doCheck = false;
 
   passthru.updateScript = final.nix-update-script {
     extraArgs = [
