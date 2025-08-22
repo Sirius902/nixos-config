fce57f895f61263d3f74ce79ae49f0eb72211334
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-22 10:49:26 -0700

    cosmic: update
    cosmic-comp: 1.0.0-alpha.7-unstable-2025-08-21 -> 1.0.0-alpha.7-unstable-2025-08-22
    
    Diff: https://github.com/pop-os/cosmic-comp/compare/f2813f0500c6af8cb1fd804c520d181607c17938...4a385d55354f3b3ca3a994d24d165e127dad8179
    
    cosmic-settings-daemon: 1.0.0-alpha.7-unstable-2025-08-11 -> 1.0.0-alpha.7-unstable-2025-08-22
    
    Diff: https://github.com/pop-os/cosmic-settings-daemon/compare/19f10525ff00d76558147ea060bd856a87122353...3f72461e99ed9acaccd3199d8888cf619dc5f511
    
    cosmic-workspaces-epoch: 1.0.0-alpha.7-unstable-2025-06-26 -> 1.0.0-alpha.7-unstable-2025-08-22
    
    Diff: https://github.com/pop-os/cosmic-workspaces-epoch/compare/30ca652b1e8c0e50ed5638e9023ceb48b2a82720...0375f28bb29e4a7c83b313735544b434cf9414ec

--- overlays/cosmic/pkgs/cosmic-comp.nix
+++ overlays/cosmic/pkgs/cosmic-comp.nix
@@ -1,12 +1,12 @@
 # TODO(Sirius902) Overlay new cosmic-comp until https://github.com/pop-os/cosmic-comp/pull/1481 makes it to nixos-unstable.
 final: prev:
 prev.cosmic-comp.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-21";
+  version = "1.0.0-alpha.7-unstable-2025-08-22";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "f2813f0500c6af8cb1fd804c520d181607c17938";
-    hash = "sha256-1+c9dTvSSAhm5kBqvtRjt1wdfos9ce0ASM32BRhFg64=";
+    rev = "4a385d55354f3b3ca3a994d24d165e127dad8179";
+    hash = "sha256-66SYK0GjKpi/vCa3+Bhqq5jgm10SjwqZax3Fjv0/JpM=";
   };
 
   cargoHash = "sha256-6IV1qjLTBs6L+yntIEhWT4xcb8slae/6F3WTRP8fDtU=";
