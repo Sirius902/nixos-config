baf6e0d307b2f31ee836540404b87405640cf6b5
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-26 15:08:07 -0700

    cosmic: update
    cosmic-comp: 1.0.0-alpha.7-unstable-2025-08-22 -> 1.0.0-alpha.7-unstable-2025-08-26
    
    Diff: https://github.com/pop-os/cosmic-comp/compare/4a385d55354f3b3ca3a994d24d165e127dad8179...2ccdb6c93d50a727fc8b00f3010b417048dbbffa
    
    cosmic-store: 1.0.0-alpha.7-unstable-2025-08-22 -> 1.0.0-alpha.7-unstable-2025-08-26
    
    Diff: https://github.com/pop-os/cosmic-store/compare/06651b6135a9500b629ebab3b7d2041e4f26a911...db805990b6b864d19d3dfff144965802da40f7d0

--- overlays/cosmic/pkgs/cosmic-comp.nix
+++ overlays/cosmic/pkgs/cosmic-comp.nix
@@ -1,15 +1,15 @@
 # TODO(Sirius902) Overlay new cosmic-comp until https://github.com/pop-os/cosmic-comp/pull/1481 makes it to nixos-unstable.
 final: prev:
 prev.cosmic-comp.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-22";
+  version = "1.0.0-alpha.7-unstable-2025-08-26";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "4a385d55354f3b3ca3a994d24d165e127dad8179";
-    hash = "sha256-66SYK0GjKpi/vCa3+Bhqq5jgm10SjwqZax3Fjv0/JpM=";
+    rev = "2ccdb6c93d50a727fc8b00f3010b417048dbbffa";
+    hash = "sha256-hj5gYpLdZWkRH0tvgZMgJBMNIteZ/tg/WKvTxKLpltw=";
   };
 
-  cargoHash = "sha256-6IV1qjLTBs6L+yntIEhWT4xcb8slae/6F3WTRP8fDtU=";
+  cargoHash = "sha256-wGq7Cdq56Hnzww+IVXCtZet4Zojj8VUmmsjsg53XHe8=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
