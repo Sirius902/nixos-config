c5a45c4205e83adc6c1e721bca81d97339532b1d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-29 09:15:26 -0700

    cosmic: update
    cosmic-greeter: 1.0.0-alpha.7-unstable-2025-08-12 -> 1.0.0-alpha.7-unstable-2025-08-29
    
    Diff: https://github.com/pop-os/cosmic-greeter/compare/7317353a32f9ed831423819d620775d4bad1db2f...f150f9e54e7646772dd2562695c07ade73587222
    
    cosmic-session: 1.0.0-alpha.7-unstable-2025-08-20 -> 1.0.0-alpha.7-unstable-2025-08-29
    
    Diff: https://github.com/pop-os/cosmic-session/compare/4c72d42731f96cf146c1ab664d0cc4f292e2527b...4598b7cdf4a2d41737d68415966a475759d34d55

--- overlays/cosmic/pkgs/cosmic-greeter.nix
+++ overlays/cosmic/pkgs/cosmic-greeter.nix
@@ -1,14 +1,14 @@
 final: prev:
 prev.cosmic-greeter.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-12";
+  version = "1.0.0-alpha.7-unstable-2025-08-29";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "7317353a32f9ed831423819d620775d4bad1db2f";
-    hash = "sha256-2MfbtEVTarhsHM3zjX1csHU/nPkxiZLT1F6bmNPrkBI=";
+    rev = "f150f9e54e7646772dd2562695c07ade73587222";
+    hash = "sha256-v2/RV6FyWdQX4uIrnhWFl0i9XNomwWLOdpuII2fnV5E=";
   };
 
-  cargoHash = "sha256-X/tSofi4aNtA5MeWCy03Tnnz3AxIF8MCZ7ofeMSWNCc=";
+  cargoHash = "sha256-J0Yj9povzqRVSdyRYp5wOyyDxP7GQP6QQ46mckuNNwU=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
--- overlays/cosmic/pkgs/cosmic-session.nix
+++ overlays/cosmic/pkgs/cosmic-session.nix
@@ -1,19 +1,27 @@
 final: prev:
 prev.cosmic-session.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-20";
+  version = "1.0.0-alpha.7-unstable-2025-08-29";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "4c72d42731f96cf146c1ab664d0cc4f292e2527b";
-    hash = "sha256-7xVOMwAYmm2G9NURvYh3+9mxq4/nCFBCL9bOlo92yNU=";
+    rev = "4598b7cdf4a2d41737d68415966a475759d34d55";
+    hash = "sha256-S5pSlbnif2i8HNAXrYY5t+Jzi1ndJNNPVRXitHI+zMU=";
   };
 
-  cargoHash = "sha256-4leO8F32O4E+fqpR0/Nj5wBcY0N00J/JdsYnPwPCWps=";
+  cargoHash = "sha256-bo46A7hS1U0cOsa/T4oMTKUTjxVCaGuFdN2qCjVHxhg=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
     hash = finalAttrs.cargoHash;
   };
+
+  # See https://github.com/pop-os/cosmic-session/commit/4598b7cdf4a2d41737d68415966a475759d34d55.
+  postPatch =
+    (prevAttrs.postPatch or "")
+    + ''
+      substituteInPlace data/start-cosmic \
+        --replace-fail '/usr/bin/gnome-keyring-daemon' "${final.gnome-keyring}/bin/gnome-keyring-daemon"
+    '';
 
   passthru =
     (prevAttrs.passthru or {})
