235afff14fca6776f69ac3114dec56c628bbeb05
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-29 01:44:01 -0700

    chore(amd): set xserver driver

--- modules/amd.nix
+++ modules/amd.nix
@@ -4,6 +4,8 @@
     enable32Bit = true;
   };
 
+  services.xserver.videoDrivers = ["amdgpu"];
+
   # FUTURE(Sirius902) See if there's any updates on this and if this is even the problem I'm having...
   # https://gitlab.freedesktop.org/drm/amd/-/issues/4356
 }
