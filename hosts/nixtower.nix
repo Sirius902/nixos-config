4f4173096f861d048cd2a250ba80669893da6d62
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-08-28 19:42:27 -0700

    feat(linux): add lts specialization

--- hosts/nixtower.nix
+++ hosts/nixtower.nix
@@ -1,5 +1,14 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [./nixlee.nix];
+
+  specialisation.lts-kernel.configuration = {
+    system.nixos.tags = ["lts"];
+    boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
+  };
 
   my.rnnoise.micNodeName = lib.mkForce "alsa_input.usb-Logitech_Yeti_GX_2428SGV014T8-00.mono-fallback";
 }
