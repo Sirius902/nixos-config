35dab3b8672a0485b5df41d59a59847a20358c69
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-03 12:41:33 -0700

    sdl3_git: 3.2.22-unstable-2025-09-02 -> 3.2.22-unstable-2025-09-03
    Diff: https://github.com/libsdl-org/SDL/compare/af74b1fe84911ef6e3f4739c1de8a442d77bd9d3...9abeeebad5b72a8f2cf94a02c01904ddbd68234a

--- pkgs/sdl3_git/package.nix
+++ pkgs/sdl3_git/package.nix
@@ -5,11 +5,11 @@
 }:
 sdl3.overrideAttrs (finalAttrs: prevAttrs: {
   pname = "${prevAttrs.pname}-git";
-  version = "3.2.22-unstable-2025-09-02";
+  version = "3.2.22-unstable-2025-09-03";
   src = prevAttrs.src.override {
     tag = null;
-    rev = "af74b1fe84911ef6e3f4739c1de8a442d77bd9d3";
-    hash = "sha256-8toGbm8GWqZ6OFnuTet43t0IDDYH+piEVUSbjDar6pE=";
+    rev = "9abeeebad5b72a8f2cf94a02c01904ddbd68234a";
+    hash = "sha256-K1nHjMqCQzRgX8Zj08IaLjz3UGCt3py6lS2E2D0GCYc=";
   };
 
   # TODO(Sirius902) NSO GC left stick calibration was broken by this commit.
