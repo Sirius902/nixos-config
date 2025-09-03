a9dbeffec121e43a7496f7b95ab53747766d492a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-03 12:41:22 -0700

    gamescope: 3.16.14.2-unstable-2025-08-29 -> 3.16.16-unstable-2025-09-03
    Diff: https://github.com/ValveSoftware/gamescope/compare/2f30679c80791844c29402d232462874fe23dd46...a51480365788b819030cc8dc6a08566a56da10de

--- overlays/gamescope.nix
+++ overlays/gamescope.nix
@@ -1,10 +1,10 @@
 final: prev: {
   gamescope = prev.gamescope.overrideAttrs (prevAttrs: {
-    version = "3.16.14.2-unstable-2025-08-29";
+    version = "3.16.16-unstable-2025-09-03";
     src = prevAttrs.src.override {
       tag = null;
-      rev = "2f30679c80791844c29402d232462874fe23dd46";
-      hash = "sha256-6bFHcZ0diZbDJ3X/dDDBTbZXaZtw8gBg9gXb1BS5V2w=";
+      rev = "a51480365788b819030cc8dc6a08566a56da10de";
+      hash = "sha256-HxdcB0dbvHV+TBqnseZPlLQ2vI0DS5VI17ROeaSjAPU=";
     };
 
     passthru =
