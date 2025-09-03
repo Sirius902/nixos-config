af85718eeed1c2b8a9f221cd33813ffb8191c120
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-03 15:45:58 -0700

    sdl3_git: 3.2.22-unstable-2025-09-03 -> 3.2.22-unstable-2025-09-03
    Diff: https://github.com/libsdl-org/SDL/compare/9abeeebad5b72a8f2cf94a02c01904ddbd68234a...637a9b34abd7ba6612e888219bacefa1bfb1c01d

--- pkgs/sdl3_git/package.nix
+++ pkgs/sdl3_git/package.nix
@@ -8,13 +8,9 @@ sdl3.overrideAttrs (finalAttrs: prevAttrs: {
   version = "3.2.22-unstable-2025-09-03";
   src = prevAttrs.src.override {
     tag = null;
-    rev = "9abeeebad5b72a8f2cf94a02c01904ddbd68234a";
-    hash = "sha256-K1nHjMqCQzRgX8Zj08IaLjz3UGCt3py6lS2E2D0GCYc=";
+    rev = "637a9b34abd7ba6612e888219bacefa1bfb1c01d";
+    hash = "sha256-HB97MX4nDqGPgHPSXuiQ/SxGg08Y0ylIXhdcvX3+MLU=";
   };
-
-  # TODO(Sirius902) NSO GC left stick calibration was broken by this commit.
-  # https://github.com/libsdl-org/SDL/commit/1f007ad5cd6f8103e8975295e6cfa9659a26cad9
-  patches = (prevAttrs.patches or []) ++ [./revert-ns2-led.patch];
 
   passthru =
     (prevAttrs.passthru or {})
