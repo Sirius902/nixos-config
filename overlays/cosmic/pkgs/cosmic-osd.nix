0cc20360ce8f4f2a3989f24e5b92eb365c58914a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-03 15:47:31 -0700

    cosmic-osd: 1.0.0-alpha.7-unstable-2025-09-02 -> 1.0.0-alpha.7-unstable-2025-09-03
    Diff: https://github.com/pop-os/cosmic-osd/compare/d69a50eafa280ef0a60153100c86217c8b965384...aa8a7d5a01489bbf56e33f45abcadf1e497d7757

--- overlays/cosmic/pkgs/cosmic-osd.nix
+++ overlays/cosmic/pkgs/cosmic-osd.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-osd.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-02";
+  version = "1.0.0-alpha.7-unstable-2025-09-03";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "d69a50eafa280ef0a60153100c86217c8b965384";
-    hash = "sha256-bToVt7cc7rIlDMYPEjRKZyiIcx//qtSMksUyXQBWXwA=";
+    rev = "aa8a7d5a01489bbf56e33f45abcadf1e497d7757";
+    hash = "sha256-UYJck+j8q+cN1aFkETNg/BE+BfJj2bV72Dw2UrCYl5Y=";
   };
 
   cargoHash = "sha256-090ifWvXb2Idd/LBybDTbmKx4QGYa4I0EDRs4yKbI4A=";
