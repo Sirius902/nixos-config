2852055c2f4ebbc29fd016d2586c5de17e058a34
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-05 12:41:44 -0700

    cosmic-session: 1.0.0-alpha.7-unstable-2025-08-29 -> 1.0.0-alpha.7-unstable-2025-09-05
    Diff: https://github.com/pop-os/cosmic-session/compare/4598b7cdf4a2d41737d68415966a475759d34d55...2441be2ad02fc28b397f67b704f2b41de7a96ed6

--- overlays/cosmic/pkgs/cosmic-session.nix
+++ overlays/cosmic/pkgs/cosmic-session.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-session.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-29";
+  version = "1.0.0-alpha.7-unstable-2025-09-05";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "4598b7cdf4a2d41737d68415966a475759d34d55";
-    hash = "sha256-S5pSlbnif2i8HNAXrYY5t+Jzi1ndJNNPVRXitHI+zMU=";
+    rev = "2441be2ad02fc28b397f67b704f2b41de7a96ed6";
+    hash = "sha256-J+r4HoxZmKU0E8eirGpLOihIvkVlmkmcXMPMkWsQuK8=";
   };
 
   cargoHash = "sha256-bo46A7hS1U0cOsa/T4oMTKUTjxVCaGuFdN2qCjVHxhg=";
@@ -14,14 +14,6 @@ prev.cosmic-session.overrideAttrs (finalAttrs: prevAttrs: {
     inherit (finalAttrs) pname src version;
     hash = finalAttrs.cargoHash;
   };
-
-  # See https://github.com/pop-os/cosmic-session/commit/4598b7cdf4a2d41737d68415966a475759d34d55.
-  postPatch =
-    (prevAttrs.postPatch or "")
-    + ''
-      substituteInPlace data/start-cosmic \
-        --replace-fail '/usr/bin/gnome-keyring-daemon' "${final.gnome-keyring}/bin/gnome-keyring-daemon"
-    '';
 
   passthru =
     (prevAttrs.passthru or {})
