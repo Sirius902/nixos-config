6f825c3cf9762310c27582982e09ef1288ef4fd7
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-05 12:26:46 -0700

    cosmic-comp: 1.0.0-alpha.7-unstable-2025-09-04 -> 1.0.0-alpha.7-unstable-2025-09-05
    Diff: https://github.com/pop-os/cosmic-comp/compare/c9e64acad2860e1068cf11468da00acc7ee2f86d...dfdb375c0a150682af47db8d8fff46664ca767ac

--- overlays/cosmic/pkgs/cosmic-comp.nix
+++ overlays/cosmic/pkgs/cosmic-comp.nix
@@ -1,12 +1,12 @@
 # TODO(Sirius902) Overlay new cosmic-comp until https://github.com/pop-os/cosmic-comp/pull/1481 makes it to nixos-unstable.
 final: prev:
 prev.cosmic-comp.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-04";
+  version = "1.0.0-alpha.7-unstable-2025-09-05";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "c9e64acad2860e1068cf11468da00acc7ee2f86d";
-    hash = "sha256-4DDdqWGmUFctMib2FNkaYc9m4ivPdBWd71Mc9XHZI+c=";
+    rev = "dfdb375c0a150682af47db8d8fff46664ca767ac";
+    hash = "sha256-NeBlnovq/St0r//bo8PSw4Iio9LQEkPul39WJkDVrHQ=";
   };
 
   cargoHash = "sha256-wGq7Cdq56Hnzww+IVXCtZet4Zojj8VUmmsjsg53XHe8=";
