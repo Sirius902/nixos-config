8eef40dba458d9248d6b1451e7ede7ab17c99709
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-07 12:44:11 -0700

    cosmic-workspaces-epoch: 1.0.0-alpha.7-unstable-2025-08-22 -> 1.0.0-alpha.7-unstable-2025-09-07
    Diff: https://github.com/pop-os/cosmic-workspaces-epoch/compare/0375f28bb29e4a7c83b313735544b434cf9414ec...05c9af1b95e55319f2b170c7d32ed05e9bd49f3c

--- overlays/cosmic/pkgs/cosmic-workspaces-epoch.nix
+++ overlays/cosmic/pkgs/cosmic-workspaces-epoch.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-workspaces-epoch.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-22";
+  version = "1.0.0-alpha.7-unstable-2025-09-07";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "0375f28bb29e4a7c83b313735544b434cf9414ec";
-    hash = "sha256-xyM3w4et92jFc0mm7XwWHGzVjc0u11F0u4p3J3s7M/s=";
+    rev = "05c9af1b95e55319f2b170c7d32ed05e9bd49f3c";
+    hash = "sha256-hDk0vTiadiriaZpFhRDHRd7+maRR0wVGXOR8Sb4HDII=";
   };
 
   cargoHash = "sha256-wFX5EReAnZ7ymXYfMfiZU1MeUUCcOKEkWdSeyGHEuKg=";
