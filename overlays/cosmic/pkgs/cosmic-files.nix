b7a5c830933eba3833cc31ed2bf2a2c9d759866f
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-07 12:38:57 -0700

    cosmic-files: 1.0.0-alpha.7-unstable-2025-08-21 -> 1.0.0-alpha.7-unstable-2025-09-07
    Diff: https://github.com/pop-os/cosmic-files/compare/f06029facec3f2dfe510663e4014074cbb85af94...27d9e5dd6bf81d05faa2d5813f19b0cc97d91b8b

--- overlays/cosmic/pkgs/cosmic-files.nix
+++ overlays/cosmic/pkgs/cosmic-files.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-files.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-08-21";
+  version = "1.0.0-alpha.7-unstable-2025-09-07";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "f06029facec3f2dfe510663e4014074cbb85af94";
-    hash = "sha256-IUcY5px1CLApOGW3Qhm7u7J182idaaZqENHzvNBsPFw=";
+    rev = "27d9e5dd6bf81d05faa2d5813f19b0cc97d91b8b";
+    hash = "sha256-F+s87i/vpNFAVmpUGzyNQ8Iy3e0yFQvJ2YoXfCt69Mc=";
   };
 
   cargoHash = "sha256-TDXo0PsDLIBewAasBK82VsG1O0lPqY6g3dBRFsGzF6A=";
