ecb3b0367e94cbcfaedbec74f8526b6368f68dd7
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-07 19:07:50 -0700

    fix(archipelago): run without nix-ld

--- pkgs/archipelago/package.nix
+++ pkgs/archipelago/package.nix
@@ -3,70 +3,101 @@
   pkgs,
   stdenvNoCC,
   appimageTools,
+  autoPatchelfHook,
   makeWrapper,
   xsel,
   xclip,
   mtdev,
+  openssl,
+  lttng-ust,
+  gobject-introspection,
   zenity,
   fetchurl,
   nix-update-script,
-}: let
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "archipelago";
   version = "0.6.3";
+
   src = fetchurl {
-    url = "https://github.com/ArchipelagoMW/Archipelago/releases/download/${version}/Archipelago_${version}_linux-x86_64.AppImage";
+    url = "https://github.com/ArchipelagoMW/Archipelago/releases/download/${finalAttrs.version}/Archipelago_${finalAttrs.version}_linux-x86_64.AppImage";
     hash = "sha256-PetlGYsdhyvThIFqy+7wbPLAXDcgN2Kcl2WF3rta8PA=";
   };
 
-  appimageContents = appimageTools.extractType2 {inherit pname version src;};
-in
-  stdenvNoCC.mkDerivation (finalAttrs: {
-    inherit pname version src;
+  dontUnpack = true;
 
-    dontUnpack = true;
+  nativeBuildInputs = [
+    autoPatchelfHook
+    makeWrapper
+  ];
 
-    nativeBuildInputs = [
-      makeWrapper
+  buildInputs = finalAttrs.runtimeDependencies;
+
+  runtimeDependencies =
+    [
+      gobject-introspection
+      openssl
+      lttng-ust
+    ]
+    ++ appimageTools.defaultFhsEnvArgs.targetPkgs pkgs
+    ++ appimageTools.defaultFhsEnvArgs.multiPkgs pkgs;
+
+  libraryPath = lib.makeLibraryPath [
+    xsel
+    xclip
+    mtdev
+  ];
+
+  binPath = lib.makeBinPath [
+    zenity
+  ];
+
+  appimageContents = appimageTools.extractType2 {inherit (finalAttrs) pname version src;};
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp ${finalAttrs.appimageContents}/AppRun $out/bin/archipelago
+
+    mkdir -p $out/lib/opt
+    cp -r ${finalAttrs.appimageContents}/opt/* $out/lib/opt
+
+    wrapProgram $out/bin/archipelago \
+      --set APPDIR $out/lib \
+      --prefix LD_LIBRARY_PATH : "${finalAttrs.libraryPath}" \
+      --prefix PATH : "${finalAttrs.binPath}"
+
+    install -Dm444 ${finalAttrs.appimageContents}/archipelago.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/archipelago.desktop \
+      --replace-fail 'opt/Archipelago/ArchipelagoLauncher' "archipelago"
+    cp -r ${finalAttrs.appimageContents}/usr/share/icons $out/share
+
+    runHook postInstall
+  '';
+
+  preFixup = ''
+    patchelf \
+      --replace-needed libcrypto.so.1.0.0 libcrypto.so \
+      --replace-needed libssl.so.1.0.0 libssl.so \
+      $out/lib/opt/Archipelago/EnemizerCLI/System.Security.Cryptography.Native.OpenSsl.so
+
+    patchelf --replace-needed liblttng-ust.so.0 liblttng-ust.so \
+      $out/lib/opt/Archipelago/EnemizerCLI/libcoreclrtraceptprovider.so
+  '';
+
+  passthru.updateScript = nix-update-script {};
+
+  meta = {
+    description = "Multi-Game Randomizer and Server";
+    homepage = "https://archipelago.gg";
+    changelog = "https://github.com/ArchipelagoMW/Archipelago/releases/tag/${finalAttrs.version}";
+    license = lib.licenses.mit;
+    mainProgram = "archipelago";
+    maintainers = with lib.maintainers; [
+      pyrox0
+      iqubic
     ];
-
-    runtimeDependencies =
-      [
-        xsel
-        xclip
-        mtdev
-      ]
-      ++ appimageTools.defaultFhsEnvArgs.targetPkgs pkgs
-      ++ appimageTools.defaultFhsEnvArgs.multiPkgs pkgs;
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      makeWrapper ${appimageContents}/AppRun $out/bin/archipelago \
-        --set APPDIR '${appimageContents}' \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath finalAttrs.runtimeDependencies} \
-        --prefix PATH : ${lib.makeBinPath [zenity]}
-
-      install -Dm444 ${appimageContents}/archipelago.desktop -t $out/share/applications
-      substituteInPlace $out/share/applications/archipelago.desktop \
-        --replace-fail 'opt/Archipelago/ArchipelagoLauncher' "archipelago"
-      cp -r ${appimageContents}/usr/share/icons $out/share
-
-      runHook postInstall
-    '';
-
-    passthru.updateScript = nix-update-script {};
-
-    meta = {
-      description = "Multi-Game Randomizer and Server";
-      homepage = "https://archipelago.gg";
-      changelog = "https://github.com/ArchipelagoMW/Archipelago/releases/tag/${version}";
-      license = lib.licenses.mit;
-      mainProgram = "archipelago";
-      maintainers = with lib.maintainers; [
-        pyrox0
-        iqubic
-      ];
-      platforms = lib.platforms.linux;
-    };
-  })
+    platforms = lib.platforms.linux;
+  };
+})
