5be00b672bcb5295efb0e58834cc891e6c4b2a29
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-11 23:05:20 -0700

    xdg-desktop-portal-cosmic: 1.0.0-alpha.7-unstable-2025-09-09 -> 1.0.0-alpha.7-unstable-2025-09-12
    Diff: https://github.com/pop-os/xdg-desktop-portal-cosmic/compare/a342b02c76f1ffd5e27e3087ad09a0759b1c2cc1...a069d57d359c4fe25a0415bdfee6c967e07b5a48

--- overlays/cosmic/pkgs/xdg-desktop-portal-cosmic.nix
+++ overlays/cosmic/pkgs/xdg-desktop-portal-cosmic.nix
@@ -2,15 +2,15 @@
 # until the nixos-unstable version is newer.
 final: prev:
 prev.xdg-desktop-portal-cosmic.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-09";
+  version = "1.0.0-alpha.7-unstable-2025-09-12";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "a342b02c76f1ffd5e27e3087ad09a0759b1c2cc1";
-    hash = "sha256-TxfrVBImrRdaEtpKumr50Jaed3KKS35ts0E3SciHCgU=";
+    rev = "a069d57d359c4fe25a0415bdfee6c967e07b5a48";
+    hash = "sha256-jwLTgzchY18rPbc93DEADmJ2XHkLBsO002YoxWbCq2Y=";
   };
 
-  cargoHash = "sha256-NQoqbfNEMWowo2KxdgKqTbn/BDgv218NFCCGYR9OAO0=";
+  cargoHash = "sha256-uJKwwESkzqweM4JunnMIsDE8xhCyjFFZs1GiJAwnbG8=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
