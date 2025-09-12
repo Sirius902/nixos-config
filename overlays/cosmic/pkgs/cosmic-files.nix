6521ad3252729ae013222175fca98e5ea009ceb2
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-11 17:56:43 -0700

    cosmic-files: 1.0.0-alpha.7-unstable-2025-09-07 -> 1.0.0-alpha.7-unstable-2025-09-11
    Diff: https://github.com/pop-os/cosmic-files/compare/27d9e5dd6bf81d05faa2d5813f19b0cc97d91b8b...6d9cd3e7e59102cd3f9d53f1326a23d1db5dbac7

--- overlays/cosmic/pkgs/cosmic-files.nix
+++ overlays/cosmic/pkgs/cosmic-files.nix
@@ -1,14 +1,14 @@
 final: prev:
 prev.cosmic-files.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-07";
+  version = "1.0.0-alpha.7-unstable-2025-09-11";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "27d9e5dd6bf81d05faa2d5813f19b0cc97d91b8b";
-    hash = "sha256-F+s87i/vpNFAVmpUGzyNQ8Iy3e0yFQvJ2YoXfCt69Mc=";
+    rev = "6d9cd3e7e59102cd3f9d53f1326a23d1db5dbac7";
+    hash = "sha256-+h3YbDqwprcftSdSBYnbNx6eusx5Im8W53/IOrBgEtk=";
   };
 
-  cargoHash = "sha256-TDXo0PsDLIBewAasBK82VsG1O0lPqY6g3dBRFsGzF6A=";
+  cargoHash = "sha256-ujdlHtbb1N4V6Qp0KW+GGM+DglA5LQyAFnZ8u38cbMg=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
