656b9ef2e83f00268264916db8144c752388edd3
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-12 20:43:31 -0700

    cosmic-greeter: 1.0.0-alpha.7-unstable-2025-09-07 -> 1.0.0-alpha.7-unstable-2025-09-12
    Diff: https://github.com/pop-os/cosmic-greeter/compare/da11207f128d0fcaf135fcebf27f286882b8204b...79bf2cb4f896b84eb3e51068b7d4941b127e0ddd

--- overlays/cosmic/pkgs/cosmic-greeter.nix
+++ overlays/cosmic/pkgs/cosmic-greeter.nix
@@ -1,14 +1,14 @@
 final: prev:
 prev.cosmic-greeter.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-07";
+  version = "1.0.0-alpha.7-unstable-2025-09-12";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "da11207f128d0fcaf135fcebf27f286882b8204b";
-    hash = "sha256-XIiYDQ+B6Qqs24EeHDCIY2gqiGOx4hTEPM+FwYSSK78=";
+    rev = "79bf2cb4f896b84eb3e51068b7d4941b127e0ddd";
+    hash = "sha256-krpoRFh6zmCwA++kaHyxnqAsmC+3vctRW718x9eAKU0=";
   };
 
-  cargoHash = "sha256-J0Yj9povzqRVSdyRYp5wOyyDxP7GQP6QQ46mckuNNwU=";
+  cargoHash = "sha256-42In98f6wLkv5rEow6ASditwfnowVTTkvR8kL7c8Dss=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
