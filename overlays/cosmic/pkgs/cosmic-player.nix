6c12f3f50e91e884cdb6e78403bdff7288b1c903
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-14 14:48:34 -0700

    cosmic-player: 1.0.0-alpha.7-unstable-2025-09-11 -> 1.0.0-alpha.7-unstable-2025-09-14
    Diff: https://github.com/pop-os/cosmic-player/compare/3dc76ba60d7dc780519de599d12e519594a30287...849dd8fabf8f3f05664157cdfa9469ceff7d65aa

--- overlays/cosmic/pkgs/cosmic-player.nix
+++ overlays/cosmic/pkgs/cosmic-player.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-player.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-11";
+  version = "1.0.0-alpha.7-unstable-2025-09-14";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "3dc76ba60d7dc780519de599d12e519594a30287";
-    hash = "sha256-rbDeCBefg0n5dYdw8ANA9Pm8prFucMsY9V/U35L78P4=";
+    rev = "849dd8fabf8f3f05664157cdfa9469ceff7d65aa";
+    hash = "sha256-18RBAhprkdlafK+mmcjDtGrzG6bCHTW89qia2yA9A7w=";
   };
 
   cargoHash = "sha256-DodFIfthiGFSvXWfPsPjFhNY6G7z3lb6pfc5HtUXhMo=";
