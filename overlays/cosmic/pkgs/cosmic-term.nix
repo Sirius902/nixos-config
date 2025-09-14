5f8b40b804751757390c50458ef40316f223208f
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-14 14:50:17 -0700

    cosmic-term: 1.0.0-alpha.7-unstable-2025-09-13 -> 1.0.0-alpha.7-unstable-2025-09-14
    Diff: https://github.com/pop-os/cosmic-term/compare/6e2b2a0755316456a2ddc903cc987da53ac657dc...b39d4ac58bb4b63b9aee66dd297f0768f3ab7259

--- overlays/cosmic/pkgs/cosmic-term.nix
+++ overlays/cosmic/pkgs/cosmic-term.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-term.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-13";
+  version = "1.0.0-alpha.7-unstable-2025-09-14";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "6e2b2a0755316456a2ddc903cc987da53ac657dc";
-    hash = "sha256-pJBexkdLMeI4VOzv6G95qPeXGo/mNf/VakT3t10fxEY=";
+    rev = "b39d4ac58bb4b63b9aee66dd297f0768f3ab7259";
+    hash = "sha256-j8LwE1p1p0CtxzzP9TLdFOrJ+x/BcT/Kyza+oqC1zkA=";
   };
 
   cargoHash = "sha256-zfby5QMDZzJhRvsf07QRBw2/DFX7BmiNByOLQegBbmo=";
