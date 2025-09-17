459ad5084926d3bc86c639a8c4566339e313b11e
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-17 11:19:09 -0700

    cosmic-protocols: 0-unstable-2025-09-01 -> 0-unstable-2025-09-17
    Diff: https://github.com/pop-os/cosmic-protocols/compare/6254f50abc6dbfccadc6939f80e20081ab5f9d51...af1997b1827ad64aab46fa31c0b77fb20d7a537a

--- overlays/cosmic/pkgs/cosmic-protocols.nix
+++ overlays/cosmic/pkgs/cosmic-protocols.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-protocols.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "0-unstable-2025-09-01";
+  version = "0-unstable-2025-09-17";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "6254f50abc6dbfccadc6939f80e20081ab5f9d51";
-    hash = "sha256-gOYgz07RGZoBp2RbHn0jUGLGXH/geoch/Y27Qh+jBao=";
+    rev = "af1997b1827ad64aab46fa31c0b77fb20d7a537a";
+    hash = "sha256-gIfCk8FqZo1iFwTTtcLqnX14Jg3k6UXIBkpKsom43EU=";
   };
 
   passthru =
