7d988dd502ef81836153ad2cb2c6e8dc0c8d96e4
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-19 21:05:00 -0700

    cosmic-comp: 1.0.0-alpha.7-unstable-2025-09-19 -> 1.0.0-beta.1-unstable-2025-09-19
    Diff: https://github.com/pop-os/cosmic-comp/compare/b19f66702fcdc9c4a7ab066e8dbca4e94bb5063e...b19f66702fcdc9c4a7ab066e8dbca4e94bb5063e

--- overlays/cosmic/pkgs/cosmic-comp.nix
+++ overlays/cosmic/pkgs/cosmic-comp.nix
@@ -1,7 +1,7 @@
 # TODO(Sirius902) Overlay new cosmic-comp until https://github.com/pop-os/cosmic-comp/pull/1481 makes it to nixos-unstable.
 final: prev:
 prev.cosmic-comp.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-19";
+  version = "1.0.0-beta.1-unstable-2025-09-19";
 
   src = prevAttrs.src.override {
     tag = null;
