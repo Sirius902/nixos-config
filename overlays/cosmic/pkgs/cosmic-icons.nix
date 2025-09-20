43ac2517bb0c25725ee5939464a44f966220ced4
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-19 21:08:05 -0700

    cosmic-icons: 1.0.0-alpha.7-unstable-2025-09-15 -> 1.0.0-beta.1-unstable-2025-09-15
    Diff: https://github.com/pop-os/cosmic-icons/compare/70b07582e24ec2114672256b9657ca80670bca8a...70b07582e24ec2114672256b9657ca80670bca8a

--- overlays/cosmic/pkgs/cosmic-icons.nix
+++ overlays/cosmic/pkgs/cosmic-icons.nix
@@ -1,6 +1,6 @@
 final: prev:
 prev.cosmic-icons.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-15";
+  version = "1.0.0-beta.1-unstable-2025-09-15";
 
   src = prevAttrs.src.override {
     tag = null;
