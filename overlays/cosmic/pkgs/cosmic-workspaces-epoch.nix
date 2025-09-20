6054d464d05837304661803ca5fdb89683d1fb9d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-19 21:14:52 -0700

    cosmic-workspaces-epoch: 1.0.0-alpha.7-unstable-2025-09-19 -> 1.0.0-beta.1-unstable-2025-09-19
    Diff: https://github.com/pop-os/cosmic-workspaces-epoch/compare/d3fc7a2815107e368d51087650e4819cc47d7828...d3fc7a2815107e368d51087650e4819cc47d7828

--- overlays/cosmic/pkgs/cosmic-workspaces-epoch.nix
+++ overlays/cosmic/pkgs/cosmic-workspaces-epoch.nix
@@ -1,6 +1,6 @@
 final: prev:
 prev.cosmic-workspaces-epoch.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-19";
+  version = "1.0.0-beta.1-unstable-2025-09-19";
 
   src = prevAttrs.src.override {
     tag = null;
