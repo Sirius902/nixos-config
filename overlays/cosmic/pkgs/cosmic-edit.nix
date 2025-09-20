1af9b8e188d2bc5a518b4bbb38f771e615b3b1cf
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-19 21:06:13 -0700

    cosmic-edit: 1.0.0-alpha.7-unstable-2025-09-17 -> 1.0.0-beta.1-unstable-2025-09-17
    Diff: https://github.com/pop-os/cosmic-edit/compare/6844f290fda0a39a254c5cf1898991039bc755a9...6844f290fda0a39a254c5cf1898991039bc755a9

--- overlays/cosmic/pkgs/cosmic-edit.nix
+++ overlays/cosmic/pkgs/cosmic-edit.nix
@@ -1,6 +1,6 @@
 final: prev:
 prev.cosmic-edit.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-alpha.7-unstable-2025-09-17";
+  version = "1.0.0-beta.1-unstable-2025-09-17";
 
   src = prevAttrs.src.override {
     tag = null;
