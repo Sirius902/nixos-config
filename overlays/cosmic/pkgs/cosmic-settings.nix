d18c53a388a2c875397d122662f0e9cdc6ad46b7
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-20 17:49:39 -0700

    cosmic-settings: 1.0.0-beta.1-unstable-2025-09-19 -> 1.0.0-beta.1-unstable-2025-09-20
    Diff: https://github.com/pop-os/cosmic-settings/compare/886220fc9f373c9a2dedb52224f24c1efdfefa6e...3d24bf151c99feb7ae53b16e2ebf95e31bc9433c

--- overlays/cosmic/pkgs/cosmic-settings.nix
+++ overlays/cosmic/pkgs/cosmic-settings.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-settings.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1-unstable-2025-09-19";
+  version = "1.0.0-beta.1-unstable-2025-09-20";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "886220fc9f373c9a2dedb52224f24c1efdfefa6e";
-    hash = "sha256-TrFo4zJKuMbTteqczrKJ8EXKuPcEsvATFIUYSGozTTA=";
+    rev = "3d24bf151c99feb7ae53b16e2ebf95e31bc9433c";
+    hash = "sha256-bnN48ynti/ApGvo2lfFfyLPH0QUWJ72q8CKi5YxHTxc=";
   };
 
   cargoHash = "sha256-dHyUTV5txSLWEDE7Blplz8CBvyuUmYNNr1kbifujHKk=";
