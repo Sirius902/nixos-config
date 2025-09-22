2043f588e8b85f4148112fbdea36c75aeba7176c
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-21 20:12:28 -0700

    zelda64recomp: pin n64recomp to fix crash

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -35,7 +35,18 @@ in rec {
 
   n64recomp = pkgs.callPackage ./n64recomp/package.nix {};
   z64decompress = pkgs.callPackage ./z64decompress/package.nix {};
-  zelda64recomp = makeNsoGcTriggersDigital "$out/bin/Zelda64Recompiled" (pkgs.callPackage ./zelda64recomp/package.nix {SDL2 = SDL2_git;});
+  zelda64recomp = makeNsoGcTriggersDigital "$out/bin/Zelda64Recompiled" (pkgs.callPackage ./zelda64recomp/package.nix {
+    SDL2 = SDL2_git;
+    # FUTURE(Sirius902) The game crashes after loading a file on the following commit, pin to the previous commit.
+    # https://github.com/N64Recomp/N64Recomp/commit/afc2ff93a5b71b3f5aac34940bb84a87d2ea7e0b
+    n64recomp = n64recomp.overrideAttrs (prevAttrs: {
+      version = "unstable-2025-09-06";
+      src = prevAttrs.src.override {
+        rev = "a49c51b37f841c9d5bec20f1eab345167f27f566";
+        hash = "sha256-kAlmCNVDTUjfA5vPb/bTMZGgXzIucD9X8/FdAGuHjJc=";
+      };
+    });
+  });
 
   archipelago = pkgs.callPackage ./archipelago/package.nix {};
 
