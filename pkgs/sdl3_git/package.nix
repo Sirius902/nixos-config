252cd81d6da7d06590d7b440c83f7491274531e9
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-21 19:13:06 -0700

    sdl3_git: 3.2.22-unstable-2025-09-21 -> 3.2.22-unstable-2025-09-22
    Diff: https://github.com/libsdl-org/SDL/compare/005f10bd97633a44b60f69236ff4d4a3c777b5b3...26dd4e3abbfb11aeefb229bae2851cee97ae8a4a

--- pkgs/sdl3_git/package.nix
+++ pkgs/sdl3_git/package.nix
@@ -9,11 +9,11 @@
 }:
 sdl3.overrideAttrs (finalAttrs: prevAttrs: {
   pname = "${prevAttrs.pname}-git";
-  version = "3.2.22-unstable-2025-09-21";
+  version = "3.2.22-unstable-2025-09-22";
   src = prevAttrs.src.override {
     tag = null;
-    rev = "005f10bd97633a44b60f69236ff4d4a3c777b5b3";
-    hash = "sha256-7qPsAUBUcCHUCGOnw+e7Znk2l9UPWdriQJxH/Go8Ozw=";
+    rev = "26dd4e3abbfb11aeefb229bae2851cee97ae8a4a";
+    hash = "sha256-6YuG95d6FRp8gxu6K8Da062ncPtRq7e7aJlQ3lwcmYE=";
   };
 
   postPatch =
