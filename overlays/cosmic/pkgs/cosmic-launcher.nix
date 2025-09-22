ecc79b71daf82a0069872bb5aec6280543390418
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-22 12:06:47 -0700

    cosmic-launcher: 1.0.0-beta.1-unstable-2025-09-21 -> 1.0.0-beta.1-unstable-2025-09-22
    Diff: https://github.com/pop-os/cosmic-launcher/compare/57fa65b5931ebfcfc06dbfdeeb72c77bed091133...b411aa6bb33399642795aca9ad002ddf0b7a7dcd

--- overlays/cosmic/pkgs/cosmic-launcher.nix
+++ overlays/cosmic/pkgs/cosmic-launcher.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-launcher.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1-unstable-2025-09-21";
+  version = "1.0.0-beta.1-unstable-2025-09-22";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "57fa65b5931ebfcfc06dbfdeeb72c77bed091133";
-    hash = "sha256-n8SxxheXJd1yQebagDI/4bB1bpG7w0E3gF8gDUYolNE=";
+    rev = "b411aa6bb33399642795aca9ad002ddf0b7a7dcd";
+    hash = "sha256-vAuWH9qmstPcfPvcKoM/2VjGxAfdtO9hhOPaZ5Ft4Y0=";
   };
 
   cargoHash = "sha256-57rkCufJPWm844/iMIfULfaGR9770q8VgZgnqCM57Zg=";
