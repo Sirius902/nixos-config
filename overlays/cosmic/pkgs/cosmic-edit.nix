bdd84d3a338a44296bb7c8a76ddfb09fb4dfa95d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-23 10:37:28 -0700

    cosmic-edit: 1.0.0-beta.1-unstable-2025-09-22 -> 1.0.0-beta.1-unstable-2025-09-23
    Diff: https://github.com/pop-os/cosmic-edit/compare/0fa5c4f056640293004fcf0f1e92e38357456d5d...5ade792fe25da3aad4eefee99ff5bf7c83a614f3

--- overlays/cosmic/pkgs/cosmic-edit.nix
+++ overlays/cosmic/pkgs/cosmic-edit.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-edit.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1-unstable-2025-09-22";
+  version = "1.0.0-beta.1-unstable-2025-09-23";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "0fa5c4f056640293004fcf0f1e92e38357456d5d";
-    hash = "sha256-yii4B1mRBFS4TBy9vr4YRhy3zX4CjxqtjaoH/C0nKkw=";
+    rev = "5ade792fe25da3aad4eefee99ff5bf7c83a614f3";
+    hash = "sha256-IspePnYrg091X5pdPQAiBmttB0bKMItHuPSntGnBf7s=";
   };
 
   cargoHash = "sha256-YfD06RAQPZRwapd0fhNsZ0tx+0JMNDXiPJIWwDhmG0M=";
