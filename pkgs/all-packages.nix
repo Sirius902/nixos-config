728915bd26d1e480b72cfe50fba435e14db08c69
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-23 01:48:29 -0700

    fix(darwin): update-all

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -45,18 +45,25 @@ in rec {
 
   n64recomp = pkgs.callPackage ./n64recomp/package.nix {};
   z64decompress = pkgs.callPackage ./z64decompress/package.nix {};
-  zelda64recomp = makeNsoGcTriggersDigital "$out/bin/Zelda64Recompiled" (pkgs.callPackage ./zelda64recomp/package.nix {
-    SDL2 = SDL2_git;
-    # FUTURE(Sirius902) The game crashes after loading a file on the following commit, pin to the previous commit.
-    # https://github.com/N64Recomp/N64Recomp/commit/afc2ff93a5b71b3f5aac34940bb84a87d2ea7e0b
-    n64recomp = n64recomp.overrideAttrs (prevAttrs: {
-      version = "0-unstable-2025-09-06";
-      src = prevAttrs.src.override {
-        rev = "a49c51b37f841c9d5bec20f1eab345167f27f566";
-        hash = "sha256-kAlmCNVDTUjfA5vPb/bTMZGgXzIucD9X8/FdAGuHjJc=";
-      };
-    });
-  });
+  zelda64recomp = let
+    pkg = pkgs.callPackage ./zelda64recomp/package.nix {
+      SDL2 = SDL2_git;
+      # FUTURE(Sirius902) The game crashes after loading a file on the following commit, pin to the previous commit.
+      # https://github.com/N64Recomp/N64Recomp/commit/afc2ff93a5b71b3f5aac34940bb84a87d2ea7e0b
+      n64recomp = n64recomp.overrideAttrs (prevAttrs: {
+        version = "0-unstable-2025-09-06";
+        src = prevAttrs.src.override {
+          rev = "a49c51b37f841c9d5bec20f1eab345167f27f566";
+          hash = "sha256-kAlmCNVDTUjfA5vPb/bTMZGgXzIucD9X8/FdAGuHjJc=";
+        };
+      });
+    };
+  in
+    # Avoid overriding nonexistent sdl_gamecontrollerdb arg on darwin.
+    # FUTURE(Sirius902) This is kinda ugly though.
+    if pkgs.stdenv.isDarwin
+    then pkg
+    else makeNsoGcTriggersDigital "$out/bin/Zelda64Recompiled" pkg;
 
   archipelago = pkgs.callPackage ./archipelago/package.nix {};
 
