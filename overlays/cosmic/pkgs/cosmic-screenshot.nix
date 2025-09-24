02d9f696cb2844d4782050463ee0278966bdad39
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-24 16:18:56 -0700

    cosmic-screenshot: 1.0.0-beta.1-unstable-2025-09-22 -> 1.0.0-beta.1-unstable-2025-09-24
    Diff: https://github.com/pop-os/cosmic-screenshot/compare/16acfc02a58a831c92e7d24aeffa0150322b3577...ba59f95cb1ef28e6836535fc6e1874b5667f76f2

--- overlays/cosmic/pkgs/cosmic-screenshot.nix
+++ overlays/cosmic/pkgs/cosmic-screenshot.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-screenshot.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1-unstable-2025-09-22";
+  version = "1.0.0-beta.1-unstable-2025-09-24";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "16acfc02a58a831c92e7d24aeffa0150322b3577";
-    hash = "sha256-psMSHpGN1p/axuDvo8X2f7my5ZkLRclSqIQbJfY1JBM=";
+    rev = "ba59f95cb1ef28e6836535fc6e1874b5667f76f2";
+    hash = "sha256-TKR8EDXZwKKC1WSZhlcf5U6tiM4cWCdb24U74vVKTaU=";
   };
 
   cargoHash = "sha256-O8fFeg1TkKCg+QbTnNjsH52xln4+ophh/BW/b4zQs9o=";
