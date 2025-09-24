0797731a811796ff0aed8c2a13251d6b4ebec303
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-09-24 16:20:22 -0700

    cosmic-store: 1.0.0-beta.1-unstable-2025-09-23 -> 1.0.0-beta.1-unstable-2025-09-24
    Diff: https://github.com/pop-os/cosmic-store/compare/f599d444bb15386c849895fc2bee0ca50226def6...57542dcafe41a575c9353d9286e53504768dd601

--- overlays/cosmic/pkgs/cosmic-store.nix
+++ overlays/cosmic/pkgs/cosmic-store.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-store.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1-unstable-2025-09-23";
+  version = "1.0.0-beta.1-unstable-2025-09-24";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "f599d444bb15386c849895fc2bee0ca50226def6";
-    hash = "sha256-06o5nqB/5Tu9CdMxfycBpIH+BISS7W0G6ihed+j88l0=";
+    rev = "57542dcafe41a575c9353d9286e53504768dd601";
+    hash = "sha256-KlXFFoUEa0YTQDEJHMrbWOIEsSnXvJNzzaEFYR83t9s=";
   };
 
   cargoHash = "sha256-xdNYQB/zmndnMAkstwJ6Z2uk0fXli3gIYHchUq/3u6k=";
