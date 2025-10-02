0329c959dc02ef1a4418fa9ef63da86414c256d0
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-02 15:10:10 -0700

    SDL2_git: 2.32.56-unstable-2025-09-29 -> 2.32.56-unstable-2025-10-02
    Diff: https://github.com/libsdl-org/sdl2-compat/compare/b86119e320fb4d6b3b1ce541dd335ba70456d8db...afa1378f10e246d0fb6833c97e29b23f703cebe4

--- pkgs/SDL2_git/package.nix
+++ pkgs/SDL2_git/package.nix
@@ -6,11 +6,11 @@
 }:
 (SDL2.override {sdl3 = sdl3_git;}).overrideAttrs (finalAttrs: prevAttrs: {
   pname = "${prevAttrs.pname}-git";
-  version = "2.32.56-unstable-2025-09-29";
+  version = "2.32.56-unstable-2025-10-02";
   src = prevAttrs.src.override {
     tag = null;
-    rev = "b86119e320fb4d6b3b1ce541dd335ba70456d8db";
-    hash = "sha256-8cORoK93dyiog+WkwTV7IBQGdRvjE/1AEVe24P1EDCo=";
+    rev = "afa1378f10e246d0fb6833c97e29b23f703cebe4";
+    hash = "sha256-IMdR676Bu1MSqWXd7cN/Y7MZPCuKTg4I3OXxhH3NCd8=";
   };
 
   passthru =
