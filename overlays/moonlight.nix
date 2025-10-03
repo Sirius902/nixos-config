3fe616a1467cd49f94a9e44c3e4d5741ed93fd4a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-03 16:14:22 -0700

    moonlight: 1.3.29 -> 1.3.32
    Diff: https://github.com/moonlight-mod/moonlight/compare/v1.3.29...v1.3.32
    
    Changelog: https://raw.githubusercontent.com/moonlight-mod/moonlight/refs/tags/v1.3.32/CHANGELOG.md

--- overlays/moonlight.nix
+++ overlays/moonlight.nix
@@ -1,11 +1,11 @@
 final: prev: {
   moonlight = prev.moonlight.overrideAttrs (prevAttrs: {
-    version = "1.3.29";
+    version = "1.3.32";
     src = prevAttrs.src.override {
-      hash = "sha256-HlSyg/ccr1AKwui1lx7aLK3ocOPGaiTVVHDB1xL+wWQ=";
+      hash = "sha256-aXap/wUjjJTCy2eq7p7BL6ZYOVZEBVY4/YkrDtbIj2Q=";
     };
     pnpmDeps = prevAttrs.pnpmDeps.override {
-      hash = "sha256-wbNAZmOqt1d243pIVM2tVdFD7BNpvPFVY3sxlYXoZCI=";
+      hash = "sha256-gv+PHGbo0IiPgcxi0RAvsNmGLHWZar3SB4eW7NbJRJY=";
     };
   });
 }
