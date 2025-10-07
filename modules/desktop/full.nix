34a2ad7277a9ae338cf223283df93c96478c3a44
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-07 12:09:10 -0700

    fix: remove use of `virtualisation.libvirtd.ovmf`

--- modules/desktop/full.nix
+++ modules/desktop/full.nix
@@ -24,16 +24,6 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          })
-          .fd
-        ];
-      };
     };
     # TODO: Use hooks.qemu instead of symlinking in libvirtd service.
     #hooks.qemu = { };
