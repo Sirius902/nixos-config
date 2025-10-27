139cd38effb5c899a1965082f3721c1776647b50
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-27 14:52:04 -0700

    cosmic-bg: 1.0.0-beta.1.1-unstable-2025-09-19 -> 1.0.0-beta.3-unstable-2025-10-12
    Diff: https://github.com/pop-os/cosmic-bg/compare/40254a7101b52b482f06d35a4d2eba8245814b2c...f1f79d33892cc47c558d33250ae6a2fc9d9dbff0

--- overlays/cosmic/pkgs/cosmic-bg.nix
+++ overlays/cosmic/pkgs/cosmic-bg.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-bg.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1.1-unstable-2025-09-19";
+  version = "1.0.0-beta.3-unstable-2025-10-12";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "40254a7101b52b482f06d35a4d2eba8245814b2c";
-    hash = "sha256-t5tY7Axiz39lCDhDC/lKSRCURfHspeaP49kNXJvCLC4=";
+    rev = "f1f79d33892cc47c558d33250ae6a2fc9d9dbff0";
+    hash = "sha256-mF6W/RND9cNfS27lQNZRcXY4OUMS+UUMMcEalBQ59Yg=";
   };
 
   cargoHash = "sha256-+NkraWjWHIMIyktAUlp3q2Ot1ib1QRsBBvfdbr5BXto=";
