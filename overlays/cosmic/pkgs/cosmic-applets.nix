26217f13654f6f264c3dfff2469dfb7e4db0c61f
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-27 14:51:13 -0700

    cosmic-applets: 1.0.0-beta.1.1-unstable-2025-09-19 -> 1.0.0-beta.3-unstable-2025-10-24
    Diff: https://github.com/pop-os/cosmic-applets/compare/2dba07fcae15ff93ea08afac111655325c1a3eca...1d565fca80de4cc23cc321f8f2d8527cc8761d71

--- overlays/cosmic/pkgs/cosmic-applets.nix
+++ overlays/cosmic/pkgs/cosmic-applets.nix
@@ -1,14 +1,14 @@
 final: prev:
 prev.cosmic-applets.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "1.0.0-beta.1.1-unstable-2025-09-19";
+  version = "1.0.0-beta.3-unstable-2025-10-24";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "2dba07fcae15ff93ea08afac111655325c1a3eca";
-    hash = "sha256-uUcEwa9rGHLzmlutmLl/e38ZqybfYMU0Dhe+FsT5V/E=";
+    rev = "1d565fca80de4cc23cc321f8f2d8527cc8761d71";
+    hash = "sha256-hBqsFiH2zKwGN9tWEf4iDoVcwssj7YN+7+2ooVNKszs=";
   };
 
-  cargoHash = "sha256-RnkyIlTJMxMGu+EsmZwvSIapSqdng+t8bqMVsDXprlU=";
+  cargoHash = "sha256-HLbcTDwS5IvolEMb0bZr4CPjtEjZI8G+AggXifIDiKM=";
 
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname src version;
