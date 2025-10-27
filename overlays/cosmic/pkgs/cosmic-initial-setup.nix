916d2e722c29b80b8634dad189bdee91a6b96f25
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-27 14:50:02 -0700

    cosmic-initial-setup: init overlay at 1.0.0-beta.3-unstable-2025-10-22

---
+++ overlays/cosmic/pkgs/cosmic-initial-setup.nix
@@ -0,0 +1,29 @@
+final: prev:
+prev.cosmic-comp.overrideAttrs (finalAttrs: prevAttrs: {
+  version = "1.0.0-beta.3-unstable-2025-10-22";
+
+  src = prevAttrs.src.override {
+    tag = null;
+    rev = "5a2eca29c38f24901a8f5f793d2190571d95c9ad";
+    hash = "sha256-vdpKxXkC6Z8ySa21jZ5zF2aqwLY7dt2FpQbfbZkgu+c=";
+  };
+
+  cargoHash = "sha256-Gk2vXmVMcDWg3VnI0176eKF65tAfPwVZ74oQtD0UBO8=";
+
+  cargoDeps = final.rustPlatform.fetchCargoVendor {
+    inherit (finalAttrs) pname src version;
+    hash = finalAttrs.cargoHash;
+    patches =
+      if builtins.hasAttr "cargoPatches" finalAttrs
+      then finalAttrs.cargoPatches
+      else null;
+  };
+
+  passthru =
+    (prevAttrs.passthru or {})
+    // {
+      updateScript = final.nix-update-script {
+        extraArgs = ["--version-regex=epoch-(.*)"];
+      };
+    };
+})
