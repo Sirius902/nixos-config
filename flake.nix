061e9427b448d4844bc70b774e1b16434bc8f4a7
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-10-27 23:51:30 -0700

    nixpkgs: use latest hydra eval

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nixlee flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=6242b0061817159818107346754f70229014bbdc";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
