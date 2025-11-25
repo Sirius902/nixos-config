925552ce803f6623c5fbd3c0d390d4d6bb517b45
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-11-25 00:28:48 -0800

    cosmic-ext-calculator: 0.2.0-unstable-2025-09-25 -> 0.2.0-unstable-2025-11-16
    Diff: https://github.com/cosmic-utils/calculator/compare/0b7670dc291a9941c230f11e9f77245ae4a82a9d...bb770fa8f562eff56c80e2f54f07c1463c77cc1b
    
    Changelog: https://github.com/cosmic-utils/calculator/releases/tag/0.1.1

--- overlays/cosmic/pkgs/cosmic-ext-calculator.nix
+++ overlays/cosmic/pkgs/cosmic-ext-calculator.nix
@@ -1,11 +1,11 @@
 final: prev:
 prev.cosmic-ext-calculator.overrideAttrs (finalAttrs: prevAttrs: {
-  version = "0.2.0-unstable-2025-09-25";
+  version = "0.2.0-unstable-2025-11-16";
 
   src = prevAttrs.src.override {
     tag = null;
-    rev = "0b7670dc291a9941c230f11e9f77245ae4a82a9d";
-    hash = "sha256-qPo+Qi6P0m3rNA6Qo6iNsgzGyirPqzXk4nj3OG6IuZ0=";
+    rev = "bb770fa8f562eff56c80e2f54f07c1463c77cc1b";
+    hash = "sha256-MEe2cQKprxbzD88f6HRSZdZfRGb8bPD6I2B/+e6MFTE=";
   };
 
   cargoHash = "sha256-Pq1E4O6lZMe+wKJgQKDBmgdsJJsJTyK0FDXU53n+Di4=";
