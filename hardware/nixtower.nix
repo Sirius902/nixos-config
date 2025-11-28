8b18d8ab429c945d34e8a85cf198e36deced5a0c
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-11-28 13:20:19 -0800

    nixtower: set tmpfs root max size to 20% RAM

--- hardware/nixtower.nix
+++ hardware/nixtower.nix
@@ -20,7 +20,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = ["defaults" "size=3G" "mode=755"];
+    options = ["defaults" "size=20%" "mode=755"];
   };
 
   fileSystems."/nix" = {
