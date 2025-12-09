521c1e27a7270c7a752f1f6ee991da541acd5448
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-12-09 13:11:11 -0800

    sdl_gamecontrollerdb: 0-unstable-2025-12-05 -> 0-unstable-2025-12-09
    Diff: https://github.com/mdqinc/SDL_GameControllerDB/compare/2b71fffae463ecaa08391c86cfd8a9f1842389af...40a55b268f2ac7f7d718addc4eba30d75bc41e3f

--- pkgs/sdl_gamecontrollerdb/package.nix
+++ pkgs/sdl_gamecontrollerdb/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sdl_gamecontrollerdb";
-  version = "0-unstable-2025-12-05";
+  version = "0-unstable-2025-12-09";
 
   src = fetchFromGitHub {
     owner = "mdqinc";
     repo = "SDL_GameControllerDB";
-    rev = "2b71fffae463ecaa08391c86cfd8a9f1842389af";
-    hash = "sha256-DD93UoxfBp95TLYnBna9Z3LmBoy4tZ5s5ZXSprP47yU=";
+    rev = "40a55b268f2ac7f7d718addc4eba30d75bc41e3f";
+    hash = "sha256-dQsoigwYpFNpTLek2mMSRq70kzouiAsxQcJkDlw3UEk=";
   };
 
   dontBuild = true;
