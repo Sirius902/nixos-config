a4a037e91c2193c26f11921e214ddb0e54d77776
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-12-14 20:23:33 -0800

    lib: add extraModules

--- lib/default.nix
+++ lib/default.nix
@@ -4,28 +4,34 @@
     host,
     nixpkgs ? inputs.nixpkgs,
     setHostName ? true,
+    extraModules ? [],
   }:
     nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = {inherit inputs;};
-      modules = [
-        (../. + "/hosts/${host}/configuration.nix")
+      modules =
+        [
+          (../. + "/hosts/${host}/configuration.nix")
 
-        ({lib, ...}: {
-          networking.hostName = lib.mkIf setHostName host;
-        })
-      ];
+          ({lib, ...}: {
+            networking.hostName = lib.mkIf setHostName host;
+          })
+        ]
+        ++ extraModules;
     };
 
   darwinSystem = {
     system,
     host,
+    extraModules ? [],
   }:
     inputs.nix-darwin.lib.darwinSystem {
       inherit system;
       specialArgs = {inherit inputs;};
-      modules = [
-        (../. + "/hosts/${host}/configuration.nix")
-      ];
+      modules =
+        [
+          (../. + "/hosts/${host}/configuration.nix")
+        ]
+        ++ extraModules;
     };
 }
