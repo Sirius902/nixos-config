448209ca2dc6c675a334d59f0875f6f21c5df32b
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-12-24 17:42:02 -0800

    wrye-bash: refactor taglists.nix

---
+++ pkgs/wrye-bash/taglists.nix
@@ -0,0 +1,38 @@
+{
+  Enderal = {
+    repository = "enderal";
+    hash = "sha256-5pIzHepPhSt5pkVkKa2eWsImJ7km2n5yQ+RXfhSdfik=";
+  };
+  Fallout3 = {
+    repository = "fallout3";
+    hash = "sha256-CDAy+Kf5bvKwqZocQ390HIyMOudqSog+g8k/+MJqMaY=";
+  };
+  FalloutNV = {
+    repository = "falloutnv";
+    hash = "sha256-Ykcnkoj6rc5NJPmVc4OnjvVRX2sXtaMjwf2vyqxWOMw=";
+  };
+  Fallout4 = {
+    repository = "fallout4";
+    hash = "sha256-xW2iIoEZgQIQKeYUgvVQhsJfazLMGSLpL/Sv1FSRs7c=";
+  };
+  Morrowind = {
+    repository = "morrowind";
+    hash = "sha256-/9NrwI31VWXprV8Xm3L/5T5EBMBuyMRnc97+NUovacY=";
+  };
+  Oblivion = {
+    repository = "oblivion";
+    hash = "sha256-xphW9BLd6JIw1jvCaj1A5F8S55es5PTE3hnGcF/hFv8=";
+  };
+  Skyrim = {
+    repository = "skyrim";
+    hash = "sha256-NwaQgUTJBD4xT7llsNLNC/XeoT1HxMzXDm+Yx5oa7GI=";
+  };
+  SkyrimSE = {
+    repository = "skyrimse";
+    hash = "sha256-rBwvKtFTZjl51u2BOSSTwX6nHxmu34gYysunZWX1sFw=";
+  };
+  Starfield = {
+    repository = "starfield";
+    hash = "sha256-bOX25usKTOwEjG61jIqlcvGJV9itKLFDcV4QE7FUgTs=";
+  };
+}
