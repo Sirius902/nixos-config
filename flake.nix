eb16c3fccad5e7042776594ebfec32c30ac0ce75
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-01-16 14:21:55 -0800

    Reapply "fix(flake): use patched nixpkgs for packages output"
    This reverts commit c6fc6f0a2856abcc144eab93411adf9e3a59dcca.

--- flake.nix
+++ flake.nix
@@ -47,37 +47,14 @@
         "aarch64-darwin"
       ];
 
-      flake = let
-        patchNixpkgs = {
-          system,
-          nixpkgs,
-        }:
-          nixpkgs.legacyPackages.${system}.applyPatches {
-            name = "nixpkgs-patched";
-            src = nixpkgs;
-            patches = [
-              # Add librepods https://github.com/NixOS/nixpkgs/pull/444137
-              (builtins.fetchurl {
-                name = "add-librepods.patch";
-                url = "https://github.com/NixOS/nixpkgs/compare/78a51b69699c3f6b366dc5c2fb62a567b8334459...1c229bf6f394e65227854061b7d7e5ffa7753ae5.patch?full_index=1";
-                sha256 = "sha256:1b8nny6k1vyyc1lnf123br5w0p006sj8r8ac65v9afk0cgvd0cay";
-              })
-              # Update rpcs3 https://github.com/NixOS/nixpkgs/pull/478577
-              (builtins.fetchurl {
-                name = "update-rpcs3.patch";
-                url = "https://github.com/NixOS/nixpkgs/commit/4310884afc21f3f4c9de97eb0aef2acfd171084d.patch?full_index=1";
-                sha256 = "sha256:074g2fjzy2ix5r0nvk0fwh98920zml6q168lbnmzz8vbx1zqnl10";
-              })
-            ];
-          };
-      in {
+      flake = {
         lib = import ./lib/default.nix {inherit inputs;};
 
         nixosConfigurations = {
           sirius-lee = let
             inherit (inputs) nixpkgs;
             system = "x86_64-linux";
-            nixpkgs' = patchNixpkgs {inherit system nixpkgs;};
+            nixpkgs' = self.lib.patchNixpkgs {inherit system nixpkgs;};
             pkgs' = import nixpkgs' {
               inherit system;
               config.allowUnfree = true;
@@ -99,7 +76,7 @@
           nixtower = let
             inherit (inputs) nixpkgs;
             system = "x86_64-linux";
-            nixpkgs' = patchNixpkgs {inherit system nixpkgs;};
+            nixpkgs' = self.lib.patchNixpkgs {inherit system nixpkgs;};
             pkgs' = import nixpkgs' {
               inherit system;
               config.allowUnfree = true;
@@ -218,6 +195,15 @@
         };
 
         packages = let
+          nixpkgs' = self.lib.patchNixpkgs {
+            inherit system;
+            inherit (inputs) nixpkgs;
+          };
+          pkgs = import nixpkgs' {
+            inherit system;
+            overlays = import ./overlays/default.nix {inherit inputs;};
+            config.allowUnfree = true;
+          };
           allPackages = import ./pkgs/all-packages.nix {inherit pkgs;};
         in
           (lib.mapAttrs (name: _: pkgs.${name}) allPackages)
