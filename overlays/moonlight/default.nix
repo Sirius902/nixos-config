ce7f0efe9fa612aff3266b0a3675bad9e8fa7585
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-01-18 14:58:32 -0800

    moonlight: bring back overlay

--- overlays/moonlight/default.nix
+++ overlays/moonlight/default.nix
@@ -5,10 +5,8 @@ final: prev: {
       hash = "sha256-4cz1icY7i8RFdh/HhG/y6UzR/zkhsp4+G2dplm4g+wo=";
     };
     pnpmDeps = prevAttrs.pnpmDeps.override {
-      hash = "sha256-pqCje7yPTasPvVuE8sf4Xb+ivaxnAIOtjB+zdpaBaoM=";
+      fetcherVersion = 3;
+      hash = "sha256-sU0EBSNwpjqyBsvyJim8Qz90dht7xc6f52HaY0sBPds=";
     };
-    patches = [
-      ./disable_updates.patch
-    ];
   });
 }
