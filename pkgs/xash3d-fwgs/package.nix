b754f55217cf3dcbe2572d5389642217bc5f903f
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-02-03 22:07:35 -0800

    xash3d-fwgs: init at 0-unstable-2026-02-08

---
+++ pkgs/xash3d-fwgs/package.nix
@@ -0,0 +1,107 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  ensureNewerSourcesForZipFilesHook,
+  python3,
+  pkg-config,
+  wafHook,
+  SDL2,
+  xorg,
+  freetype,
+  opusfile,
+  libopus,
+  libogg,
+  libvorbis,
+  bzip2,
+  xash-sdk,
+  makeWrapper,
+  nix-update-script,
+  # Options
+  buildXashSdk ? true,
+}:
+stdenv.mkDerivation {
+  pname = "xash3d-fwgs";
+  version = "0-unstable-2026-02-08";
+
+  src = fetchFromGitHub {
+    owner = "FWGS";
+    repo = "xash3d-fwgs";
+    fetchSubmodules = true;
+    rev = "d9221c8491761094a002dfdaff6d21aef0bc5657";
+    hash = "sha256-+yheDTI2BCs6Vmpc5RUVewmQ45p8POyRUX3Oz1wh2QA=";
+  };
+
+  nativeBuildInputs = [
+    ensureNewerSourcesForZipFilesHook
+    python3
+    pkg-config
+    wafHook
+    makeWrapper
+  ];
+
+  buildInputs =
+    [
+      freetype
+      opusfile
+      libopus
+      libogg
+      libvorbis
+      bzip2
+      SDL2
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      xorg.libX11
+    ];
+
+  dontAddPrefix = true;
+
+  wafConfigureFlags =
+    [
+      "-T release"
+      "--sdl-use-pkgconfig"
+    ]
+    ++ lib.optionals stdenv.buildPlatform.is64bit ["-8"];
+
+  CFLAGS = "-I${SDL2.dev}/include/SDL2";
+
+  preInstall = ''
+    mkdir -p $out/lib
+  '';
+
+  wafInstallFlags = ["--destdir=${placeholder "out"}/lib"];
+
+  postInstall =
+    ''
+      mkdir -p $out/opt
+      mv $out/lib/valve $out/opt
+
+      mkdir -p $out/bin
+      mv $out/lib/xash3d $out/bin/xash3d-unwrapped
+      makeWrapper $out/bin/xash3d-unwrapped $out/bin/xash3d \
+        --set XASH3D_RODIR $out/opt/valve \
+        --run "export XASH3D_BASEDIR=\$HOME/.xash3d/" \
+        --prefix ${
+        if stdenv.hostPlatform.isDarwin
+        then "DYLD_LIBRARY_PATH"
+        else "LD_LIBRARY_PATH"
+      } : "$out/lib"
+    ''
+    + lib.optionalString buildXashSdk ''cp -TR ${xash-sdk}/valve $out/opt/valve'';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version=branch"
+      "--version-regex=(0-unstable-.*)"
+    ];
+  };
+
+  meta = {
+    homepage = "https://github.com/FWGS/xash3d-fwgs";
+    description = "Xash3D FWGS engine";
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [r4v3n6101];
+    mainProgram = "xash3d";
+  };
+}
