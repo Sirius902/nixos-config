b98b7ce34600083b532ed67d5dd6abed52b8733a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-02-12 16:32:11 -0800

    moonlight: 2026.2.0 -> 2026.2.1
    Diff: https://github.com/moonlight-mod/moonlight/compare/v2026.2.0...v2026.2.1
    
    Changelog: https://raw.githubusercontent.com/moonlight-mod/moonlight/refs/tags/v2026.2.1/CHANGELOG.md

--- overlays/moonlight/default.nix
+++ overlays/moonlight/default.nix
@@ -1,12 +1,12 @@
 final: prev: {
   moonlight = prev.moonlight.overrideAttrs (prevAttrs: {
-    version = "2026.2.0";
+    version = "2026.2.1";
     src = prevAttrs.src.override {
-      hash = "sha256-VqO3pUqnKP2VE1I/HtuY6bF/k/ijh5CBeE35Mpp6rGo=";
+      hash = "sha256-BpTN9AdQEDD2XnEUsUxgkoq+EPGhtnYgJhLKF4GVZoc=";
     };
     pnpmDeps = prevAttrs.pnpmDeps.override {
       fetcherVersion = 3;
-      hash = "sha256-mSuk829qkNq+dDDympXCBzqMUJc4L9zqzcmVZwfbXsE=";
+      hash = "sha256-b3d8VcfQjCkcJThebXJ2yvKZfU8u4QnpZgNyqP6XIu0=";
     };
   });
 }
