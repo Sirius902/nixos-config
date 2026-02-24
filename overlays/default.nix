99f3b261d40baa7c2c19779fc1e883483db9634c
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-02-24 01:15:58 -0800

    krita: fix build failure

--- overlays/default.nix
+++ overlays/default.nix
@@ -280,4 +280,10 @@
         };
       });
   })
+
+  # TODO(Sirius902) brokeded
+  # https://github.com/NixOS/nixpkgs/issues/493431
+  (final: prev: {
+    lager = prev.lager.override {boost = final.boost188;};
+  })
 ]
