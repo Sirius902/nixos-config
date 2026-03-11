281a43638175b3aeaa1f2c983f81749eb83b1b04
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-03-11 14:22:41 -0700

    moonlight: 2026.3.0 -> 2026.3.1
    Diff: https://github.com/moonlight-mod/moonlight/compare/v2026.3.0...v2026.3.1
    
    Changelog: https://raw.githubusercontent.com/moonlight-mod/moonlight/refs/tags/v2026.3.1/CHANGELOG.md

--- overlays/moonlight/default.nix
+++ overlays/moonlight/default.nix
@@ -1,8 +1,8 @@
 final: prev: {
   moonlight = prev.moonlight.overrideAttrs (prevAttrs: {
-    version = "2026.3.0";
+    version = "2026.3.1";
     src = prevAttrs.src.override {
-      hash = "sha256-Tuv0IFhvZzvmf29EWNtrb5Y6YOCn+lIBzXpt7lfLVS8=";
+      hash = "sha256-v4GAFP5cN9UXzqG+JVGlqnTQBKyXB2/cUXiOvleuFDE=";
     };
     pnpmDeps = prevAttrs.pnpmDeps.override {
       fetcherVersion = 3;
