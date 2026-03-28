c2c3bb039773cb464e6c563dd1ce477849af2a11
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-03-27 19:25:22 -0700

    xash-sdk-theyhunger: 0-unstable-2025-08-17 -> 0-unstable-2026-03-26
    Diff: https://github.com/FWGS/hlsdk-portable/compare/48ebea48f4f324d1a5855b8964fa947d59d69e05...cf57ec1294b020a49742ee626cea59d7512f03cd

--- pkgs/xash-sdk/mods/theyhunger.nix
+++ pkgs/xash-sdk/mods/theyhunger.nix
@@ -4,11 +4,11 @@
 }:
 xash-sdk.overrideAttrs (prevAttrs: {
   pname = prevAttrs.pname + "-theyhunger";
-  version = "0-unstable-2025-08-17";
+  version = "0-unstable-2026-03-26";
 
   src = prevAttrs.src.override {
-    rev = "48ebea48f4f324d1a5855b8964fa947d59d69e05";
-    hash = "sha256-BZkfmcFe+dSFnzTpicy5cuAL8jZy2r/LD7/mfg7QyH0=";
+    rev = "cf57ec1294b020a49742ee626cea59d7512f03cd";
+    hash = "sha256-iLa1cNSV7xj91652O3yZ2YkSBBgWpOmzQQ/hYR/5g8g=";
   };
 
   passthru =
