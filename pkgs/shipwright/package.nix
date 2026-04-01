f387a385649d45215e628dc9df5bf7a528a8061d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-03-31 18:08:19 -0700

    shipwright: 9.2.1-unstable-2026-03-30 -> 9.2.1-unstable-2026-04-01
    Diff: https://github.com/HarbourMasters/Shipwright/compare/7363e1c2640f5d9b9c031c25162dc02495926f82...0d2454ed656c8afb43a0e4d3e83b24f4676ae0ca

--- pkgs/shipwright/package.nix
+++ pkgs/shipwright/package.nix
@@ -114,13 +114,13 @@
 in
   stdenv.mkDerivation (finalAttrs: {
     pname = "shipwright";
-    version = "9.2.1-unstable-2026-03-30";
+    version = "9.2.1-unstable-2026-04-01";
 
     src = fetchFromGitHub {
       owner = "HarbourMasters";
       repo = "Shipwright";
-      rev = "7363e1c2640f5d9b9c031c25162dc02495926f82";
-      hash = "sha256-V3DXxNgFwZy2XuQccfL29HILe8jLS9QDxRB4ntaBAks=";
+      rev = "0d2454ed656c8afb43a0e4d3e83b24f4676ae0ca";
+      hash = "sha256-RyHs2V5Pt2hNOlwKABFYa2WnbJcXfxnNAyHXGcu7p8M=";
       fetchSubmodules = true;
       deepClone = true;
       postFetch = ''
