87ba10f89d4e1ef174da67142956391fccb9735c
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-04-19 19:28:05 -0700

    Tralsebook-V2: enable linux-builder

--- hosts/Tralsebook-V2/configuration.nix
+++ hosts/Tralsebook-V2/configuration.nix
@@ -1,7 +1,27 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../../modules/darwin/minimal.nix
   ];
+
+  nix = {
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      config = {
+        virtualisation = {
+          cores = 8;
+          darwin-builder.memorySize = 8 * 1024;
+          diskSize = lib.mkForce (40 * 1024);
+        };
+      };
+    };
+    # NOTE(Sirius902) Required for linux-builder
+    settings.trusted-users = ["@admin"];
+  };
 
   environment.systemPackages = [
     pkgs.shipwright
