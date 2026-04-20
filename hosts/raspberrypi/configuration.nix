a81f52745a56ec1da645349690392e1b2b26a37a
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-04-19 19:30:13 -0700

    Revert "raspberrypi: cross compile sops-install-secrets"
    This reverts commit a001ca438be5be9ddf55c3ef184ead726ab443d4.
    
    If I can use `nix.linux-builder` from darwin I guess I don't need this.

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -9,16 +9,6 @@
     ./hardware-configuration.nix
   ];
 
-  # Cross-compile sops-install-secrets because building with QEMU will not
-  # finish before the heat death of the universe
-  sops.package = let
-    pkgsCross = import inputs.nixpkgs {
-      localSystem = "x86_64-linux";
-      crossSystem = "aarch64-linux";
-    };
-  in
-    (pkgsCross.callPackage inputs.sops-nix {}).sops-install-secrets;
-
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
