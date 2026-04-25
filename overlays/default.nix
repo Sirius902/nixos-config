d8f090bc1bb7d4e3b79fdf8d0581f88eda5ed7ab
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-04-25 11:27:39 -0700

    shadps4-qt: 0-unstable-2026-04-22 -> 0-unstable-2026-04-25
    Diff: https://github.com/shadps4-emu/shadps4-qtlauncher/compare/7aea3285948953fced55a2321a911c358a0c3cfd...5c0e5bff316a63118839dc1637b1a3dea0a05d0d

--- overlays/default.nix
+++ overlays/default.nix
@@ -347,12 +347,12 @@
   (
     final: prev: {
       shadps4-qt = prev.shadps4-qt.overrideAttrs (prevAttrs: {
-        version = "0-unstable-2026-04-22";
+        version = "0-unstable-2026-04-25";
 
         src = prevAttrs.src.override {
           tag = null;
-          rev = "7aea3285948953fced55a2321a911c358a0c3cfd";
-          hash = "sha256-7U9BQ64LecV4ZGEKZNqMV700LMIMX7q72NUrbxslJu0=";
+          rev = "5c0e5bff316a63118839dc1637b1a3dea0a05d0d";
+          hash = "sha256-E7BjGsN0Y6ZEthfiRKEy1NMmG5p8YEJCTeZ71cEqBQQ=";
           fetchSubmodules = false;
 
           leaveDotGit = false;
