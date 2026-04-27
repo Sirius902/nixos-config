76ead1208f120ae10e57e2dd2e4c7f81caed3217
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-04-27 16:12:09 -0700

    moonlight: use new disable_updates.patch

--- overlays/moonlight/default.nix
+++ overlays/moonlight/default.nix
@@ -4,6 +4,13 @@ final: prev: {
     src = prevAttrs.src.override {
       hash = "sha256-jbIdFHPomN0zD2I6UoClofvSNVdOqpf0nM1s5pbn7ew=";
     };
+    patches = [
+      (final.fetchurl {
+        name = "disable_updates.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/df14c8e0a9120b0702e89fed5b2c908de6e0b9f4/pkgs/by-name/mo/moonlight/disable_updates.patch";
+        sha256 = "sha256-J2CCzLd9TEhdmEt3Zlh2cbKFx8kapROMpAEZVCcQjVg=";
+      })
+    ];
     pnpmDeps = prevAttrs.pnpmDeps.override {
       fetcherVersion = 3;
       hash = "sha256-veZx/b+cvpcRh1xXO8Y34dJtY2cgncqVSYYywb85Geo=";
