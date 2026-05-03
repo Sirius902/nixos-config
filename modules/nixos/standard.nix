2fdbfef39cbc8581feee17bb0b921dd485f52e6b
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-05-03 13:58:41 -0700

    zfs: set forceImportRoot to false

--- modules/nixos/standard.nix
+++ modules/nixos/standard.nix
@@ -28,6 +28,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.zfs.forceImportRoot = false;
   services.zfs.autoScrub.enable = true;
 
   systemd.tmpfiles.rules = [
