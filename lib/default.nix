3e88b991f5670f5ae6efa526eae7a8502ee02afa
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-05-12 22:27:16 -0700

    lib: move zfs patch to sirius-lee

--- lib/default.nix
+++ lib/default.nix
@@ -17,9 +17,10 @@ in {
     host,
     setHostName ? true,
     extraModules ? [],
+    extraPatches ? _pkgs: [],
   }: let
     patchedSrc = inputs.self.lib.patchNixpkgs {
-      inherit system;
+      inherit system extraPatches;
       inherit (inputs) nixpkgs;
     };
     patchedPkgs = import patchedSrc (self.lib.nixpkgsConfig system);
@@ -44,9 +45,10 @@ in {
     system,
     host,
     extraModules ? [],
+    extraPatches ? _pkgs: [],
   }: let
     patchedSrc = inputs.self.lib.patchNixpkgs {
-      inherit system;
+      inherit system extraPatches;
       inherit (inputs) nixpkgs;
     };
     patchedPkgs = import patchedSrc (self.lib.nixpkgsConfig system);
@@ -69,44 +71,41 @@ in {
   patchNixpkgs = {
     system,
     nixpkgs,
+    extraPatches ? _pkgs: [],
   }: let
     pkgs = nixpkgs.legacyPackages.${builtins.currentSystem or system};
   in
     pkgs.applyPatches {
       name = "nixpkgs-patched";
       src = nixpkgs;
-      patches = [
-        # Add shadps4-qt https://github.com/NixOS/nixpkgs/pull/474696
-        (pkgs.fetchpatch2 {
-          name = "shadps4-qt.patch";
-          url = "https://github.com/ryand56/nixpkgs/compare/2d4672af3da4241781d1e8f1483619450fa6155f...306c7efa8796bca0611b5434d53880d698e258ac.patch?full_index=1";
-          hash = "sha256-xkjfmRy9rJRwWxst/AXp1Imj7UtanR2CgIbWW1ojbj4=";
-          excludes = ["pkgs/by-name/sh/shadps4/package.nix"];
-        })
-        # TODO(Sirius902) shadps4-qt patch does not apply cleanly...
-        (pkgs.fetchpatch2 {
-          name = "shadps4-fixes.patch";
-          url = "https://github.com/Sirius902/nixpkgs/compare/a0c87fd9a4a7d620fc31adf1bab143afd9a6712c...6fc9aeda805963a2dd67e9485f7e1f48fd7af76e.patch?full_index=1";
-          hash = "sha256-WUJrdtpLDTwnSMDHMYwuuFXNDfnreM8yM12eiPNkPMM=";
-        })
-        # Update pure-prompt https://github.com/NixOS/nixpkgs/pull/491292
-        (pkgs.fetchpatch2 {
-          name = "update-pure-prompt.patch";
-          url = "https://github.com/Sirius902/nixpkgs/commit/36fca490d613fd05e006583340b8512f00cc118d.patch?full_index=1";
-          hash = "sha256-zNZK/Q4x72FRpHReGHuI3YYyNry9yrnJGnmdxlxpZyI=";
-        })
-        # Add cosmic-ext-applet-clipboard-manager https://github.com/NixOS/nixpkgs/pull/496706
-        (pkgs.fetchpatch2 {
-          name = "add-cosmic-ext-applet-clipboard-manager.patch";
-          url = "https://github.com/kritdass/nixpkgs/commit/71f8f21a50192425577f92f97eb5212a85dd0588.patch?full_index=1";
-          hash = "sha256-TvrGKoaPnrkIZyntbv/C6m55e2p2kLECtxH7/fViXM4=";
-        })
-        # Bump ZFS version
-        (pkgs.fetchpatch2 {
-          name = "zfs-bump.patch";
-          url = "https://github.com/Sirius902/nixpkgs/compare/85eb70255b4311b60ec4cb6379907da0fcfdf230...922a53036b2d883c0f816f6697bda784655c0599.patch?full_index=1";
-          hash = "sha256-mBZ4MBiJAM9SJrz9By/cCa5kde3HK+QhKVTSJx3RhI4=";
-        })
-      ];
+      patches =
+        [
+          # Add shadps4-qt https://github.com/NixOS/nixpkgs/pull/474696
+          (pkgs.fetchpatch2 {
+            name = "shadps4-qt.patch";
+            url = "https://github.com/ryand56/nixpkgs/compare/2d4672af3da4241781d1e8f1483619450fa6155f...306c7efa8796bca0611b5434d53880d698e258ac.patch?full_index=1";
+            hash = "sha256-xkjfmRy9rJRwWxst/AXp1Imj7UtanR2CgIbWW1ojbj4=";
+            excludes = ["pkgs/by-name/sh/shadps4/package.nix"];
+          })
+          # TODO(Sirius902) shadps4-qt patch does not apply cleanly...
+          (pkgs.fetchpatch2 {
+            name = "shadps4-fixes.patch";
+            url = "https://github.com/Sirius902/nixpkgs/compare/a0c87fd9a4a7d620fc31adf1bab143afd9a6712c...6fc9aeda805963a2dd67e9485f7e1f48fd7af76e.patch?full_index=1";
+            hash = "sha256-WUJrdtpLDTwnSMDHMYwuuFXNDfnreM8yM12eiPNkPMM=";
+          })
+          # Update pure-prompt https://github.com/NixOS/nixpkgs/pull/491292
+          (pkgs.fetchpatch2 {
+            name = "update-pure-prompt.patch";
+            url = "https://github.com/Sirius902/nixpkgs/commit/36fca490d613fd05e006583340b8512f00cc118d.patch?full_index=1";
+            hash = "sha256-zNZK/Q4x72FRpHReGHuI3YYyNry9yrnJGnmdxlxpZyI=";
+          })
+          # Add cosmic-ext-applet-clipboard-manager https://github.com/NixOS/nixpkgs/pull/496706
+          (pkgs.fetchpatch2 {
+            name = "add-cosmic-ext-applet-clipboard-manager.patch";
+            url = "https://github.com/kritdass/nixpkgs/commit/71f8f21a50192425577f92f97eb5212a85dd0588.patch?full_index=1";
+            hash = "sha256-TvrGKoaPnrkIZyntbv/C6m55e2p2kLECtxH7/fViXM4=";
+          })
+        ]
+        ++ (extraPatches pkgs);
     };
 }
