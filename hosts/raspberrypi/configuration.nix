f2aef84bf2900410354d0a1acfd0e84229140209
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-05-14 11:23:39 -0700

    raspberrypi: re-enable home-manager

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -7,6 +7,13 @@
     inputs.nixos-hardware.nixosModules.raspberry-pi-3
     ../../modules/nixos/base.nix
     ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = {inherit inputs;};
+      home-manager.users.chris = import ../../modules/home/default.nix;
+    }
   ];
 
   boot.loader.grub.enable = false;
