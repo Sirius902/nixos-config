b5ab215b3c562e13a987d32acd372f2d98be9614
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-05-31 14:09:55 -0700

    raspberrypi: add system.build.updateFirmware

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -19,6 +19,28 @@
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
+  system.build.updateFirmware = let
+    fw = "${pkgs.raspberrypifw}/share/raspberrypi/boot";
+    uboot = pkgs.ubootRaspberryPi3_64bit;
+  in
+    pkgs.writeShellScript "update-firmware" ''
+      set -euo pipefail
+      fwdir=/boot/firmware
+
+      echo "Backing up $fwdir to $fwdir.bak..."
+      rm -rf "$fwdir.bak"
+      cp -a "$fwdir" "$fwdir.bak"
+
+      echo "Updating firmware..."
+      cp ${fw}/bootcode.bin ${fw}/start.elf ${fw}/fixup.dat "$fwdir/"
+      cp ${fw}/bcm2710-rpi-3-b.dtb "$fwdir/"
+      cp ${uboot}/u-boot.bin "$fwdir/u-boot-rpi3.bin"
+
+      sync
+      echo "Done. Reboot to use new firmware."
+      echo "If boot fails, restore from $fwdir.bak"
+    '';
+
   hardware.enableRedistributableFirmware = true;
 
   networking.hostId = "4786fd98";
