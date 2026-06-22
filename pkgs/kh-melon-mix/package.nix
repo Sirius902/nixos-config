f39bfd06bddc0508d31f0b61a139e20c9a870292
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-06-22 15:52:26 -0700

    kh-melon-mix: 0.8.2-unstable-2026-06-18 -> 0.8.2-unstable-2026-06-22
    Diff: https://github.com/vitor251093/KHMelonMix/compare/b479b3492e15f264105bf330b8eba283ba8d9dba...e4eaf799c7e5ec6804a7831ac975cfd74d0d1419

--- pkgs/kh-melon-mix/package.nix
+++ pkgs/kh-melon-mix/package.nix
@@ -8,13 +8,13 @@
 }:
 melonds.overrideAttrs (prevAttrs: {
   pname = "kh-melon-mix";
-  version = "0.8.2-unstable-2026-06-18";
+  version = "0.8.2-unstable-2026-06-22";
 
   src = fetchFromGitHub {
     owner = "vitor251093";
     repo = "KHMelonMix";
-    rev = "b479b3492e15f264105bf330b8eba283ba8d9dba";
-    hash = "sha256-ViZ41dkadCe5y58q6D2aiVwJv8lfA9KegPACAmhMxzo=";
+    rev = "e4eaf799c7e5ec6804a7831ac975cfd74d0d1419";
+    hash = "sha256-qNcX7HbMZARybeTjNXvUX5b89rbKlexRsX0i7a6xFoo=";
   };
 
   nativeBuildInputs = (prevAttrs.nativeBuildInputs or []) ++ [makeWrapper];
