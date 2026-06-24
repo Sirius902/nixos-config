2eea59cf4ce3b1c88e48edcf9905bca64d48f675
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-06-24 13:06:28 -0700

    claude-code: 2.1.187 -> 2.1.190
    Changelog: https://github.com/anthropics/claude-code/blob/main/CHANGELOG.md

--- overlays/claude-code/default.nix
+++ overlays/claude-code/default.nix
@@ -1,12 +1,12 @@
 final: prev: let
   baseUrl = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases";
-  version = "2.1.187";
+  version = "2.1.190";
   platformKey = "${prev.stdenv.hostPlatform.node.platform}-${prev.stdenv.hostPlatform.node.arch}";
   checksums = {
-    "darwin-arm64" = "a59a16ba4922adab7a145728f215d042184d349f5f7e72cddb7fc114250a4ce3";
-    "darwin-x64" = "7f57b6935b4246d03cb7acee90dc22153083483a267da589c5c920dd04744c36";
-    "linux-arm64" = "b49be8a5e565bf2d45b50d2de62017b25462131acc9425d2fdb98b8f29c9dce2";
-    "linux-x64" = "bb02fcb33626f8c599d10d8bee38585d4cf8d4225c3b497869dee7454e7bf361";
+    "darwin-arm64" = "1fa59529c233914fdd9d42816a74ecf300eefa14c3d118d4ecba2f0f16fc5741";
+    "darwin-x64" = "4eca48431a43c5540c53657964be604f301d38c02b244ca7c05da18f84bb5c85";
+    "linux-arm64" = "e7305203e7d78a6bfecb94f7973b0ee4a71a3ba67c8028c98b293cf571900b68";
+    "linux-x64" = "0684e28517cc785ab8d19feb5dad3381eab4abc97bf6fce07bc534dc88040b27";
   };
 in {
   claude-code = prev.claude-code.overrideAttrs (prevAttrs: {
