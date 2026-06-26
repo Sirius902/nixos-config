4ed108f87f2133dc3e07bb02b1b10304331bdb4e
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-06-26 02:09:22 -0700

    dusklight: drop aurora patch

--- pkgs/dusklight/package.nix
+++ pkgs/dusklight/package.nix
@@ -95,10 +95,6 @@ in
       fetchSubmodules = true;
     };
 
-    patches = [
-      ../../patches/dusklight/aurora-restore-fifo-drains.patch
-    ];
-
     postPatch = ''
       sed -i '/add_subdirectory(tests)/d' extern/aurora/CMakeLists.txt
 
