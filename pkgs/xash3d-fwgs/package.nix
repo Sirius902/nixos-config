5c283e16a09bca967380984dcc9bfab1e98c61d2
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-08-13 07:35:49 -0700

    chore: stdenv.is* -> stdenv.hostPlatform.is*

--- pkgs/xash3d-fwgs/package.nix
+++ pkgs/xash3d-fwgs/package.nix
@@ -62,7 +62,7 @@ in
         bzip2
         SDL2
       ]
-      ++ lib.optionals (!buildServer && stdenv.isLinux) [
+      ++ lib.optionals (!buildServer && stdenv.hostPlatform.isLinux) [
         libx11
       ];
 
