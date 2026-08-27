9bfa0b6671049b307430f71ea684bef22fd6fc4d
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-08-26 19:04:31 -0700

    codex: 0.149.1 -> 0.150.1
    Diff: https://github.com/openai/codex/compare/rust-v0.149.1...rust-v0.150.1
    
    Changelog: https://raw.githubusercontent.com/openai/codex/refs/tags/rust-v0.150.1/CHANGELOG.md

--- overlays/codex/default.nix
+++ overlays/codex/default.nix
@@ -11,14 +11,14 @@ in {
     (prev.codex.override {
       inherit librusty_v8 librusty_v8_src_binding;
     }).overrideAttrs (finalAttrs: prevAttrs: {
-      version = "0.149.1";
+      version = "0.150.1";
 
       src = prevAttrs.src.override {
         tag = "rust-v${finalAttrs.version}";
-        hash = "sha256-nRJ48yuIkgHfIZQQY8vXW3oQEOCCoHACz5AsaIkI2ms=";
+        hash = "sha256-l8H2//SpzzUvtA3IZ9BXhWBqlaatbFk6rBYeLThozb4=";
       };
 
-      cargoHash = "sha256-K58PL588Hhk75FyXgU6b8IEAco8FIz8oGd1S0WgOjyQ=";
+      cargoHash = "sha256-85Po+Pe2xKltxznAv1MhC7BULJ+6AfXwM4jkZQ6+utk=";
       cargoDeps = prevAttrs.cargoDeps.overrideAttrs {
         vendorStaging = prevAttrs.cargoDeps.vendorStaging.overrideAttrs {
           outputHash = finalAttrs.cargoHash;
