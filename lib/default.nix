4c1873db005e1d58a5aeed82f10dca1aa98c2519
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-08-26 19:03:01 -0700

    Revert "zfs: update to 2.3.9 and 2.4.4"
    This reverts commit b84fd82a7b5ae7831cea8aebf9a59acac98855af.

--- lib/default.nix
+++ lib/default.nix
@@ -87,12 +87,6 @@ in {
             url = "https://github.com/NixOS/nixpkgs/commit/71f8f21a50192425577f92f97eb5212a85dd0588.diff";
             hash = "sha256-TvrGKoaPnrkIZyntbv/C6m55e2p2kLECtxH7/fViXM4=";
           })
-          # Update zfs_2_3 and zfs_2_4 https://github.com/NixOS/nixpkgs/pull/555231
-          (pkgs.fetchpatch {
-            name = "update-zfs_2_3-and-zfs_2_4.patch";
-            url = "https://github.com/NixOS/nixpkgs/compare/08e80383c24733c0357fe61f8edba64c84505e89...787089672e6e4f297e626c7802b8d6f212fed915.diff";
-            hash = "sha256-+2bW8uVIojB+ONnG17wHLbgCkiwW1ylCaYcdr7JqheQ=";
-          })
         ]
         ++ (extraPatches pkgs);
     };
