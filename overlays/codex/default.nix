1c00aff1999b5e8a85703eed4af3ce79089b2d65
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-09-04 13:15:34 -0700

    codex: 0.153.1 -> 0.153.3
    Diff: https://github.com/openai/codex/compare/rust-v0.153.1...rust-v0.153.3
    
    Changelog: https://raw.githubusercontent.com/openai/codex/refs/tags/rust-v0.153.3/CHANGELOG.md

--- overlays/codex/default.nix
+++ overlays/codex/default.nix
@@ -11,11 +11,11 @@ in {
     (prev.codex.override {
       inherit librusty_v8 librusty_v8_src_binding;
     }).overrideAttrs (finalAttrs: prevAttrs: {
-      version = "0.153.1";
+      version = "0.153.3";
 
       src = prevAttrs.src.override {
         tag = "rust-v${finalAttrs.version}";
-        hash = "sha256-u7bp0B3MUuPIlk1QUKz267EGedjwMgTaoSNoYI5piLQ=";
+        hash = "sha256-JujjJx9GHcTgirqEFr9tc4Ghzx65YNOqpNCc7rtthfI=";
       };
 
       cargoHash = "sha256-GG6kOXmCdq+bZLU2ul0DIVL8lDuweayvZvXn6+bcUZw=";
