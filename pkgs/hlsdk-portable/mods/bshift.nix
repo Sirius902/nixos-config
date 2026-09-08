579ccdb06835ebda292f0ee58a34b698f5f6a4fa
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-09-08 11:12:56 -0700

    hlsdk-portable-bshift: 0-unstable-2026-06-14 -> 0-unstable-2026-09-08
    Diff: https://github.com/FWGS/hlsdk-portable/compare/cd04b6190b234b27abc31dd992947af3842f6d24...fe56297222e138ccd681e7e235b91d39e065fde0

--- pkgs/hlsdk-portable/mods/bshift.nix
+++ pkgs/hlsdk-portable/mods/bshift.nix
@@ -4,11 +4,11 @@
 }:
 hlsdk-portable.overrideAttrs (prevAttrs: {
   pname = prevAttrs.pname + "-bshift";
-  version = "0-unstable-2026-06-14";
+  version = "0-unstable-2026-09-08";
 
   src = prevAttrs.src.override {
-    rev = "cd04b6190b234b27abc31dd992947af3842f6d24";
-    hash = "sha256-t0gm4y3jdY+421w2Xyw7XkWA4UdkEbTxCJIU6o8Zsqg=";
+    rev = "fe56297222e138ccd681e7e235b91d39e065fde0";
+    hash = "sha256-ZUPVwhi5koAoSwGDo4PaxWzLbKfksX0ddux1cIYFo4o=";
   };
 
   passthru =
