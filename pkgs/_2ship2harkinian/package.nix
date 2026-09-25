60a085c0469801d62105f9881135b1cbc4b96541
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-09-24 17:28:08 -0700

    _2ship2harkinian: 5.0.1-unstable-2026-09-18 -> 5.0.1-unstable-2026-09-16
    Diff: https://github.com/HarbourMasters/2ship2harkinian/compare/61384c2c83e3c8b4d3e29535d7e98af84a9361d5...8531560498d246214fb9ac91c428bd2b1fc00c52

--- pkgs/_2ship2harkinian/package.nix
+++ pkgs/_2ship2harkinian/package.nix
@@ -120,13 +120,13 @@ stdenv.mkDerivation (finalAttrs: let
   };
 in {
   pname = "2ship2harkinian";
-  version = "5.0.1-unstable-2026-09-18";
+  version = "5.0.1-unstable-2026-09-16";
 
   src = fetchFromGitHub {
     owner = "HarbourMasters";
     repo = "2ship2harkinian";
-    rev = "61384c2c83e3c8b4d3e29535d7e98af84a9361d5";
-    hash = "sha256-JVA3yLBJXwYnft4pHeXR69AUGdjtxAzDSuaBZTg+2Ok=";
+    rev = "8531560498d246214fb9ac91c428bd2b1fc00c52";
+    hash = "sha256-6e5M4DH1DfVByMUBM8n2OSztCM73lPfAUac9zmaqc80=";
     fetchSubmodules = true;
     deepClone = true;
     postFetch = ''
